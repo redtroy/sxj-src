a737db8b92be6dc8ec5a6e76eb4d607651dd82a6
redtroy <redtroy@sxj.com>
2014-08-18 13:01:46 +0800

    Signed-off-by: redtroy <redtroy@sxj.com>

---
+++ cache.parent/cache.core/src/main/java/com/sxj/cache/core/CacheExpiredListener.java
@@ -0,0 +1,16 @@
+package com.sxj.cache.core;
+
+/**
+ * 侦听缓存中的某个记录超时
+ * @author winterlau
+ */
+public interface CacheExpiredListener {
+
+	/**
+	 * 当缓存中的某个对象超时被清除的时候触发
+	 * @param region
+	 * @param key
+	 */
+	public void notifyElementExpired(String region, Object key) ;
+
+}
