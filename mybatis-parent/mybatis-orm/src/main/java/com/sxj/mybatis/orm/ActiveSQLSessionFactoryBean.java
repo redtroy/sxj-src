e05506c507b8dc73685611844e9c5dc23dbc7184
redtroy <redtroy@sxj.com>
2014-08-19 19:12:33 +0800

    message

--- mybatis-parent/mybatis-orm/src/main/java/com/sxj/mybatis/orm/ActiveSQLSessionFactoryBean.java
+++ mybatis-parent/mybatis-orm/src/main/java/com/sxj/mybatis/orm/ActiveSQLSessionFactoryBean.java
@@ -17,6 +17,11 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.scannotation.AnnotationDB;
 import org.scannotation.ClasspathUrlFinder;
+import org.scannotation.WarUrlFinder;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.sxj.mybatis.orm.annotations.Entity;
 import com.sxj.mybatis.orm.builder.GenericStatementBuilder;
@@ -24,36 +29,54 @@ import com.sxj.mybatis.orm.builder.GenericStatementBuilder;
 /**
  * Class description goes here.
  */
-public class ActiveSQLSessionFactoryBean extends SqlSessionFactoryBean {
-
-	private Configuration configuration;
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		super.afterPropertiesSet();
-		SqlSessionFactory sqlSessionFactory = super.getObject();
-		configuration = sqlSessionFactory.getConfiguration();
-
-		for (String clazzName : findEntityClassNames()) {
-			GenericStatementBuilder builder = new GenericStatementBuilder(configuration,
-					Class.forName(clazzName));
-			builder.build();
-		}
-	}
-
-	@Override
-	public SqlSessionFactory getObject() throws Exception {
-		return super.getObject();
-	}
-
-	private Set<String> findEntityClassNames() throws IOException {
-		AnnotationDB annotationDB = new AnnotationDB();
-		annotationDB.setScanClassAnnotations(true);
-		annotationDB.scanArchives(ClasspathUrlFinder.findClassPaths());
-		Map<String, Set<String>> map = annotationDB.getAnnotationIndex();
-		Set<String> classNames = map.get(Entity.class.getName());
-
-		return classNames;
-	}
-
+public class ActiveSQLSessionFactoryBean extends SqlSessionFactoryBean
+        implements ApplicationContextAware
+{
+    
+    private Configuration configuration;
+    
+    private ApplicationContext applicationContext;
+    
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        super.afterPropertiesSet();
+        SqlSessionFactory sqlSessionFactory = super.getObject();
+        configuration = sqlSessionFactory.getConfiguration();
+        
+        for (String clazzName : findEntityClassNames())
+        {
+            GenericStatementBuilder builder = new GenericStatementBuilder(
+                    configuration, Class.forName(clazzName));
+            builder.build();
+        }
+    }
+    
+    @Override
+    public SqlSessionFactory getObject() throws Exception
+    {
+        return super.getObject();
+    }
+    
+    private Set<String> findEntityClassNames() throws IOException
+    {
+        AnnotationDB annotationDB = new AnnotationDB();
+        annotationDB.setScanClassAnnotations(true);
+        if (applicationContext instanceof WebApplicationContext)
+            annotationDB.scanArchives(WarUrlFinder.findWebInfLibClasspaths(((WebApplicationContext) applicationContext).getServletContext()));
+        else
+            annotationDB.scanArchives(ClasspathUrlFinder.findClassPaths());
+        Map<String, Set<String>> map = annotationDB.getAnnotationIndex();
+        Set<String> classNames = map.get(Entity.class.getName());
+        
+        return classNames;
+    }
+    
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException
+    {
+        this.applicationContext = applicationContext;
+    }
+    
 }
