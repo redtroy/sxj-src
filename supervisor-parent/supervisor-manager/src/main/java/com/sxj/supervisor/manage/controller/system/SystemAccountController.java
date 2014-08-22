a965fc77f3cb4e13ac89608447b6676fdedb0d61
redtroy <redtroy@sxj.com>
2014-08-22 18:58:45 +0800

    Change-Id: I481a86fad81e5fe05f460f08be7671e4c9394a06

--- supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/system/SystemAccountController.java
+++ supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/system/SystemAccountController.java
@@ -17,38 +17,40 @@ import com.sxj.util.persistent.ResultList;
 
 @Controller
 @RequestMapping("/system")
-public class SystemAccountController extends BaseController {
-
-	@Autowired
-	private ISystemAccountService accountService;
-
-	@Autowired
-	private IFunctionService functionService;
-
-	@RequestMapping("account-list")
-	public String getSysAccountList(SysAccountQuery query, ModelMap map) {
-		ResultList<SystemAccountEntity> list = accountService
-				.queryAccounts(query);
-		List<FunctionEntity> functionList = functionService
-				.queryChildrenFunctions("0");
-		map.put("list", list.getResults());
-		map.put("functions", functionList);
-		map.put("query", query);
-		return "manage/system/account-list";
-
-	}
-
-	@RequestMapping("account-info")
-	public String getgetSysAccount(String accountId, ModelMap map) {
-		SystemAccountEntity account = accountService.getAccount(accountId);
-		map.put("account", account);
-		return "manage/system/account-info";
-	}
-	
-	@RequestMapping("to_edit")
-	public String toEditAccount(String accountId, ModelMap map) {
-		SystemAccountEntity account = accountService.getAccount(accountId);
-		map.put("account", account);
-		return "manage/system/account-edit";
-	}
+public class SystemAccountController extends BaseController
+{
+    
+    @Autowired
+    private ISystemAccountService accountService;
+    
+    @Autowired
+    private IFunctionService functionService;
+    
+    @RequestMapping("account-list")
+    public String getSysAccountList(SysAccountQuery query, ModelMap map)
+    {
+        ResultList<SystemAccountEntity> list = accountService.queryAccounts(query);
+        List<FunctionEntity> functionList = functionService.queryChildrenFunctions("0");
+        map.put("list", list.getResults());
+        map.put("functions", functionList);
+        map.put("query", query);
+        return "manage/system/account-list";
+        
+    }
+    
+    @RequestMapping("account-info")
+    public String getgetSysAccount(String accountId, ModelMap map)
+    {
+        SystemAccountEntity account = accountService.getAccount(accountId);
+        map.put("account", account);
+        return "manage/system/account-info";
+    }
+    
+    @RequestMapping("to_edit")
+    public String toEditAccount(String accountId, ModelMap map)
+    {
+        SystemAccountEntity account = accountService.getAccount(accountId);
+        map.put("account", account);
+        return "manage/system/account-edit";
+    }
 }
