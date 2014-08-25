9c4f10cf9b438c604446abf1e6bdb844bc96cd4c
redtroy <redtroy@sxj.com>
2014-08-25 14:32:15 +0800

    Change-Id: I78e0194006bd74708584834467d43a2efc2002ee

--- supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/entity/contract/ReplenishBatchEntity.java
+++ supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/entity/contract/ReplenishBatchEntity.java
@@ -19,98 +19,113 @@ import com.sxj.supervisor.dao.contract.IContractBatchDao;
  */
 @Entity(mapper = IContractBatchDao.class)
 @Table(name = "M_CONTRACT_REPLENISH_BATCH")
-public class ReplenishBatchEntity extends Pagable implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5666599729742676725L;
-
-	/**
-	 * 主键
-	 **/
-	@Id(column = "ID")
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private String id;
-	/**
-	 * 批次号
-	 */
-	@Column(name = "BATCH_NO")
-	private String batchNo;
-
-	/**
-	 * 变更ID
-	 **/
-	@Column(name = "REPLENISH_ID")
-	private String replenishId;
-
-	/**
-	 * RFID号
-	 **/
-	@Column(name = "RFID_NO")
-	private String rfidNo;
-	/**
-	 * RFID号
-	 **/
-	@Column(name = "NEW_RFID_NO")
-	private String newRfidNo;
-	/**
-	 * 金额
-	 **/
-	@Column(name = "AMOUNT")
-	private Long amount;
-
-	/**
-	 * 批次条目JSON
-	 **/
-	@Column(name = "BATCH_ITEMS")
-	private String batchItems;
-
-	public String getReplenishId() {
-		return replenishId;
-	}
-
-	public void setReplenishId(String replenishId) {
-		this.replenishId = replenishId;
-	}
-
-	public String getBatchNo() {
-		return batchNo;
-	}
-
-	public void setBatchNo(String batchNo) {
-		this.batchNo = batchNo;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-
-	public String getRfidNo() {
-		return rfidNo;
-	}
-
-	public void setRfidNo(String rfidNo) {
-		this.rfidNo = rfidNo;
-	}
-
-	public Long getAmount() {
-		return amount;
-	}
-
-	public void setAmount(Long amount) {
-		this.amount = amount;
-	}
-
-	public String getBatchItems() {
-		return batchItems;
-	}
-
-	public void setBatchItems(String batchItems) {
-		this.batchItems = batchItems;
-	}
+public class ReplenishBatchEntity extends Pagable implements Serializable
+{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5666599729742676725L;
+    
+    /**
+     * 主键
+     **/
+    @Id(column = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    
+    /**
+     * 批次号
+     */
+    @Column(name = "BATCH_NO")
+    private String batchNo;
+    
+    /**
+     * 变更ID
+     **/
+    @Column(name = "REPLENISH_ID")
+    private String replenishId;
+    
+    /**
+     * RFID号
+     **/
+    @Column(name = "RFID_NO")
+    private String rfidNo;
+    
+    /**
+     * RFID号
+     **/
+    @Column(name = "NEW_RFID_NO")
+    private String newRfidNo;
+    
+    /**
+     * 金额
+     **/
+    @Column(name = "AMOUNT")
+    private Long amount;
+    
+    /**
+     * 批次条目JSON
+     **/
+    @Column(name = "BATCH_ITEMS")
+    private String batchItems;
+    
+    public String getReplenishId()
+    {
+        return replenishId;
+    }
+    
+    public void setReplenishId(String replenishId)
+    {
+        this.replenishId = replenishId;
+    }
+    
+    public String getBatchNo()
+    {
+        return batchNo;
+    }
+    
+    public void setBatchNo(String batchNo)
+    {
+        this.batchNo = batchNo;
+    }
+    
+    public String getId()
+    {
+        return id;
+    }
+    
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+    
+    public String getRfidNo()
+    {
+        return rfidNo;
+    }
+    
+    public void setRfidNo(String rfidNo)
+    {
+        this.rfidNo = rfidNo;
+    }
+    
+    public Long getAmount()
+    {
+        return amount;
+    }
+    
+    public void setAmount(Long amount)
+    {
+        this.amount = amount;
+    }
+    
+    public String getBatchItems()
+    {
+        return batchItems;
+    }
+    
+    public void setBatchItems(String batchItems)
+    {
+        this.batchItems = batchItems;
+    }
 }
