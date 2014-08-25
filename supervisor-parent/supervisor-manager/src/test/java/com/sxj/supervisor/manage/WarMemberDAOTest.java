d7343d97c5b43b95e451ed854b16d1cbf20857b3
qjj <qjj@126.com>
2014-08-25 16:57:53 +0800

    Signed-off-by: qjj <qjj@126.com>

--- supervisor-parent/supervisor-manager/src/test/java/com/sxj/supervisor/manage/WarMemberDAOTest.java
+++ supervisor-parent/supervisor-manager/src/test/java/com/sxj/supervisor/manage/WarMemberDAOTest.java
@@ -10,36 +10,32 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 
 import com.sxj.supervisor.dao.member.IMemberDao;
 import com.sxj.supervisor.entity.member.MemberEntity;
-import com.sxj.supervisor.entity.member.MemberTypeEnum;
+import com.sxj.supervisor.enu.member.MemberTypeEnum;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/applicationContext.xml" })
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 @ActiveProfiles("test")
-public class WarMemberDAOTest
-{
-    @Autowired
-    private IMemberDao memberDao;
-    
-    @Test
-    public void testUpdateMember()
-    {
-        MemberEntity member = new MemberEntity();
-        member.setId("1");
-        member.setType(MemberTypeEnum.DAWP);
-        memberDao.updateMember(member);
-        System.out.println(member.getAddress());
-    }
-    
-    @Test
-    public void testGetMemeber()
-    {
-        memberDao.getMember("1");
-    }
-    
-    public void setMemberDao(IMemberDao memberDao)
-    {
-        this.memberDao = memberDao;
-    }
-    
+public class WarMemberDAOTest {
+	@Autowired
+	private IMemberDao memberDao;
+
+	@Test
+	public void testUpdateMember() {
+		MemberEntity member = new MemberEntity();
+		member.setId("1");
+		member.setType(MemberTypeEnum.DAWP);
+		memberDao.updateMember(member);
+		System.out.println(member.getAddress());
+	}
+
+	@Test
+	public void testGetMemeber() {
+		memberDao.getMember("1");
+	}
+
+	public void setMemberDao(IMemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
 }
