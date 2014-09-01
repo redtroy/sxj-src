946ead727bb5a2b6f9173a1cd1172a5fe3398e9a
qjj <qjj@126.com>
2014-09-01 15:43:50 +0800

    Signed-off-by: qjj <qjj@126.com>

--- supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/member/MemberController.java
+++ supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.sxj.supervisor.website.controller.member;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sxj.supervisor.entity.member.MemberEntity;
+import com.sxj.supervisor.entity.system.AreaEntity;
+import com.sxj.supervisor.enu.member.MemberCheckStateEnum;
+import com.sxj.supervisor.enu.member.MemberStatesEnum;
 import com.sxj.supervisor.enu.member.MemberTypeEnum;
 import com.sxj.supervisor.service.member.IMemberService;
+import com.sxj.supervisor.service.system.IAreaService;
 import com.sxj.supervisor.website.controller.BaseController;
 import com.sxj.supervisor.website.login.SupervisorPrincipal;
 
@@ -22,7 +27,10 @@ import com.sxj.supervisor.website.login.SupervisorPrincipal;
 public class MemberController extends BaseController {
 
 	@Autowired
-	IMemberService memberService;
+	private IMemberService memberService;
+
+	@Autowired
+	private IAreaService areaService;
 
 	/**
 	 * 根据会员号获取会员信息
@@ -80,7 +88,9 @@ public class MemberController extends BaseController {
 	@RequestMapping("/regist")
 	public String regist(ModelMap map) {
 		MemberTypeEnum[] type = MemberTypeEnum.values();
+		List<AreaEntity> list = areaService.getChildrenAreas("32");
 		map.put("type", type);
+		map.put("list", list);
 		return "site/register";
 	}
 
@@ -89,7 +99,24 @@ public class MemberController extends BaseController {
 	 */
 	@RequestMapping("regist_save")
 	public String regist_save(MemberEntity member) {
+		member.setState(MemberStatesEnum.stop);
+		member.setCheckState(MemberCheckStateEnum.unaudited);
 		memberService.addMember(member);
-		return "";
+		return LOGIN;
+	}
+
+	/**
+	 * 验证会员是否注册过
+	 */
+	@RequestMapping("check_member")
+	public @ResponseBody Map<String, String> check_member(String name) {
+		Map<String, String> map = new HashMap<String, String>();
+		MemberEntity member = memberService.getMemberByName(name);
+		if (member == null) {
+			return null;
+		} else {
+			map.put("text", "该会员已存在！");
+			return map;
+		}
 	}
 }
