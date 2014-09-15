a91aa602863092a1c459f750811626d18628bea9
qjj <qjj@126.com>
2014-09-15 11:01:38 +0800

    me

---
+++ rfid-parent/rfid-service/src/main/java/com/sxj/supervisor/service/rfid/IRfidSupplierService.java
@@ -0,0 +1,40 @@
+package com.sxj.supervisor.service.rfid;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sxj.supervisor.entity.rfid.RfidSupplierEntity;
+import com.sxj.supervisor.model.rfid.RfidSupplierQuery;
+import com.sxj.util.exception.ServiceException;
+
+@Service
+public interface IRfidSupplierService {
+	/**
+	 * 根据条件高级查询
+	 * 
+	 * @param query
+	 * @return
+	 * @throws ServiceException
+	 */
+	public List<RfidSupplierEntity> querySupplier(RfidSupplierQuery query)
+			throws ServiceException;
+
+	/**
+	 * 根据ID查询
+	 * 
+	 * @param id
+	 * @return
+	 * @throws ServiceException
+	 */
+	public RfidSupplierEntity getSupplier(String id) throws ServiceException;
+
+	/**
+	 * 更新
+	 * 
+	 * @param Supplier
+	 * @throws ServiceException
+	 */
+	public void modifySupplier(RfidSupplierEntity Supplier)
+			throws ServiceException;
+}
