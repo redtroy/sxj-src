999fe4791429dfa9aa7a29c2be12c0b182049fa8
redtroy <redtroy@sxj.com>
2014-09-16 11:56:49 +0800

    Change-Id: I1168ea87d7b190f7c76e91bcefe224a9b48f6a6e

--- supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/rfid/RfidSupplierController.java
+++ supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/rfid/RfidSupplierController.java
@@ -15,41 +15,50 @@ import com.sxj.util.exception.WebException;
 
 @Controller
 @RequestMapping("/rfid/supplier")
-public class RfidSupplierController extends BaseController {
-	@Autowired
-	IRfidSupplierService supplierService;
-
-	/**
-	 * 进入供应商管理页面
-	 * 
-	 * @return
-	 */
-	@RequestMapping("supplier_list")
-	public String supplier_list(ModelMap map, RfidSupplierQuery query)
-			throws WebException {
-		try {
-			if (query != null) {
-				query.setPagable(true);
-			}
-			List<RfidSupplierEntity> list = supplierService
-					.querySupplier(query);
-			map.put("list", list);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "manage/rfid/supplier/supplier";
-	}
-
-	/**
-	 * 增加供应商页面
-	 */
-	@RequestMapping("add_supplier")
-	public String add_supplier() throws WebException {
-		try {
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "manage/rfid/supplier/supplier-add";
-	}
+public class RfidSupplierController extends BaseController
+{
+    @Autowired
+    IRfidSupplierService supplierService;
+    
+    /**
+     * 进入供应商管理页面
+     * 
+     * @return
+     */
+    @RequestMapping("supplier_list")
+    public String supplier_list(ModelMap map, RfidSupplierQuery query)
+            throws WebException
+    {
+        try
+        {
+            if (query != null)
+            {
+                query.setPagable(true);
+            }
+            List<RfidSupplierEntity> list = supplierService.querySupplier(query);
+            map.put("list", list);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return "manage/rfid/supplier/supplier";
+    }
+    
+    /**
+     * 增加供应商页面
+     */
+    @RequestMapping("add_supplier")
+    public String add_supplier() throws WebException
+    {
+        try
+        {
+            
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return "manage/rfid/supplier/supplier-add";
+    }
 }
