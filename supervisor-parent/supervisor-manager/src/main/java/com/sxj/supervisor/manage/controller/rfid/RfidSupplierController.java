7e8aeb7a1c0f7ffc0b81484ca5534f66fe2ae525
qjj <qjj@126.com>
2014-09-16 10:19:11 +0800

    Signed-off-by: qjj <qjj@126.com>

--- supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/rfid/RfidSupplierController.java
+++ supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/rfid/RfidSupplierController.java
@@ -5,16 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sxj.supervisor.manage.controller.BaseController;
+import com.sxj.supervisor.service.rfid.base.IRfidSupplierService;
 
 @Controller
 @RequestMapping("/rfid/supplier")
 public class RfidSupplierController extends BaseController {
 	@Autowired
-	// IRfidSupplierService supplierService;
+	IRfidSupplierService supplierService;
+
+	/**
+	 * 进入供应商管理页面
+	 * 
+	 * @return
+	 */
 	@RequestMapping("supplier_list")
 	public String supplier_list() {
 
 		return "manage/rfid/supplier/supplier";
 	}
 
+	/**
+	 * 增加供应商页面
+	 */
+	@RequestMapping("add_supplier")
+	public String add_supplier() {
+		return "manage/rfid/supplier/supplier-add";
+	}
 }
