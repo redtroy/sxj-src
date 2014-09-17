43b97b246a4bd01bf44c0f083cdc966b8bf48956
qjj <qjj@126.com>
2014-09-17 17:09:17 +0800

    me

--- supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/rfid/RfidApplicationController.java
+++ supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/rfid/RfidApplicationController.java
@@ -1,12 +1,19 @@
 package com.sxj.supervisor.manage.controller.rfid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sxj.supervisor.manage.controller.BaseController;
+import com.sxj.supervisor.model.rfid.app.RfidApplicationQuery;
 
 @Controller
 @RequestMapping("/rfid/Application")
 public class RfidApplicationController extends BaseController {
 
+	@RequestMapping("appList")
+	public String appList(ModelMap map, RfidApplicationQuery query) {
+
+		return "manage/rfid/order/order";
+	}
 }
