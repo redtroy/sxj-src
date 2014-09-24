d8a28bbffe157d3fc230ce0a0b1574ad73017bc2
Ann <Ann@An-PC>
2014-09-24 18:48:17 +0800

    Signed-off-by: Ann <Ann@An-PC>

---
+++ rfid-parent/rfid-dao/src/main/java/com/sxj/supervisor/dao/rfid/windowRef/IWindowRfidRefDao.java
@@ -0,0 +1,52 @@
+package com.sxj.supervisor.dao.rfid.windowRef;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.sxj.mybatis.orm.annotations.Get;
+import com.sxj.mybatis.orm.annotations.Insert;
+import com.sxj.mybatis.orm.annotations.Update;
+import com.sxj.supervisor.entity.rfid.window.WindowRfidEntity;
+import com.sxj.util.persistent.QueryCondition;
+/**
+ * 门窗RFID关联Dao
+ * @author Ann
+ *
+ */
+public interface IWindowRfidRefDao {
+	/**
+	 * 查询门窗RFID列表关联
+	 *
+	 * @param query
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<WindowRfidEntity> queryWindowRfidRefList(
+			QueryCondition<WindowRfidEntity> query) throws SQLException;
+
+	/**
+	 * 新增门窗RFID关联
+	 *
+	 * @param member
+	 **/
+	@Insert
+	public void addWindowRfidRef(WindowRfidEntity window)
+			throws SQLException;
+
+	/**
+	 * 更新门窗RFID关联
+	 *
+	 * @param member
+	 **/
+	@Update
+	public void updateWindowRfidRef(WindowRfidEntity window)
+			throws SQLException;
+
+	/**
+	 * 获取门窗RFID关联信息
+	 *
+	 * @param id
+	 **/
+	@Get
+	public WindowRfidEntity getWindowRfidRef(String id);
+}
