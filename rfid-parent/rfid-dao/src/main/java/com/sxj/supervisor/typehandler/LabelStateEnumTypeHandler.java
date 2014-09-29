e82341a98e3af10e0863eb8a6635f225d91575a2
Ann <Ann@An-PC>
2014-09-29 18:10:20 +0800

    Signed-off-by: Ann <Ann@An-PC>

---
+++ rfid-parent/rfid-dao/src/main/java/com/sxj/supervisor/typehandler/LabelStateEnumTypeHandler.java
@@ -0,0 +1,14 @@
+package com.sxj.supervisor.typehandler;
+
+import org.apache.ibatis.type.EnumOrdinalTypeHandler;
+
+import com.sxj.supervisor.enu.rfid.logistics.LabelStateEnum;
+
+
+public class LabelStateEnumTypeHandler  extends
+		EnumOrdinalTypeHandler<LabelStateEnum> {
+	public LabelStateEnumTypeHandler(Class<LabelStateEnum> type) {
+		super(type);
+	}
+
+}
