80871b04a2f500a5675fc31dbc198f231fe7cff9
djx <dujinxin1330@163.com>
2014-10-21 14:49:01 +0800

    Change-Id: I7dc723ab1d92f27eedb1e824b7df2e271459d536

--- supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/member/AccountController.java
+++ supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/controller/member/AccountController.java
@@ -11,31 +11,88 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sxj.supervisor.entity.member.AccountEntity;
+import com.sxj.supervisor.entity.member.MemberEntity;
+import com.sxj.supervisor.entity.member.MemberFunctionEntity;
 import com.sxj.supervisor.enu.member.AccountStatesEnum;
-import com.sxj.supervisor.enu.member.MemberAuthorityEnum;
+import com.sxj.supervisor.enu.member.MemberTypeEnum;
 import com.sxj.supervisor.manage.controller.BaseController;
 import com.sxj.supervisor.model.member.AccountQuery;
+import com.sxj.supervisor.model.member.MemberFunctionModel;
 import com.sxj.supervisor.service.member.IAccountService;
+import com.sxj.supervisor.service.member.IMemberFunctionService;
+import com.sxj.supervisor.service.member.IMemberRoleService;
+import com.sxj.supervisor.service.member.IMemberService;
 
 @Controller
 @RequestMapping("/member/account")
 public class AccountController extends BaseController {
 	@Autowired
-	IAccountService accountService;
+	private IAccountService accountService;
+
+	@Autowired
+	private IMemberFunctionService functionService;
+
+	@Autowired
+	private IMemberService memberService;
+
+	@Autowired
+	private IMemberRoleService roleService;
 
 	@RequestMapping("accountList")
 	public String memberList(AccountQuery query, ModelMap map) {
 		if (query != null) {
 			query.setPagable(true);
 		}
-		MemberAuthorityEnum[] mas = MemberAuthorityEnum.values();
 		AccountStatesEnum[] states = AccountStatesEnum.values();
 		List<AccountEntity> list = accountService.queryAccounts(query);
-		map.put("mas", mas);
+		List<MemberFunctionEntity> functionList = functionService
+				.queryChildrenFunctions("0");
+		map.put("functions", functionList);
 		map.put("states", states);
 		map.put("list", list);
 		map.put("query", query);
 		return "manage/member/account";
+	}
+
+	@RequestMapping("get_role_function")
+	public String getRloeFunctions(String memberNo, String accountId,
+			String type, ModelMap map) {
+		AccountEntity account = accountService.getAccount(accountId);
+		if (account != null) {
+			memberNo = account.getParentId();
+		}
+		MemberEntity member = memberService.memberInfo(memberNo);
+		int flag = 0;
+		if (MemberTypeEnum.DAWP.equals(member.getType())) {
+			flag = 1;
+		} else {
+			flag = 2;
+		}
+		if ("get".equals(type)) {
+			List<MemberFunctionModel> list = roleService
+					.getRoleFunctions(accountId);
+			if (list != null) {
+				map.put("list", list);
+			}
+			return "manage/member/role_function";
+		} else if ("add".equals(type)) {
+			List<MemberFunctionModel> allList = functionService
+					.queryFunctions(flag);
+			map.put("allList", allList);
+			return "site/member/edit_role";
+
+		} else if ("edit".equals(type)) {
+			List<MemberFunctionEntity> list = roleService
+					.getAllRoleFunction(accountId);
+			List<MemberFunctionModel> allList = functionService
+					.queryFunctions(flag);
+			map.put("allList", allList);
+			map.put("roleList", list);
+			return "site/member/edit_role";
+		} else {
+			return null;
+		}
+
 	}
 
 	/**
