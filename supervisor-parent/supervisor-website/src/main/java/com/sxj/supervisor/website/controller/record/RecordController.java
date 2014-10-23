37cc0ba9f202a3278076f04c9d6bea672f5f67e4
Ann <Ann@An-PC>
2014-10-23 19:31:18 +0800

    Change-Id: I4d86cb9e192fc33eebd3623e0a9a9a87fc9da9cc

--- supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/record/RecordController.java
+++ supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/record/RecordController.java
@@ -301,6 +301,10 @@ public class RecordController extends BaseController {
 			RecordEntity record = recordService.getRecord(recordId);
 			SupervisorPrincipal member = (SupervisorPrincipal) session
 					.getAttribute("userinfo");
+			if(record.getType().getId()==2){
+				String batch = recordService.getBatch(recordId);
+				map.put("batch", batch);
+			}
 			map.put("record", record);// 备案类型
 			map.put("type", member.getMember().getType().getId());// 会员类型
 			return "site/record/edit-record";
