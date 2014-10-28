4585de12397b3456db6efe1947cc69e334505791
redtroy <redtroy@sxj.com>
2014-10-28 17:58:59 +0800

    Change-Id: I83450cc25aa7263a9e4b1e252896ff9bd0881870

--- supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/member/MemberController.java
+++ supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/member/MemberController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -31,217 +32,300 @@ import com.sxj.util.logger.SxjLogger;
 
 @Controller
 @RequestMapping("/member")
-public class MemberController extends BaseController {
-
-	@Autowired
-	private IMemberService memberService;
-
-	@Autowired
-	private IAreaService areaService;
-
-	/**
-	 * 根据会员号获取会员信息
-	 * 
-	 * @param map
-	 * @return
-	 * @throws WebException
-	 */
-	@RequestMapping("/memberInfo")
-	public String memberList(ModelMap map, HttpSession session,
-			HttpServletRequest request) throws WebException {
-		try {
-			SupervisorPrincipal info = getLoginInfo(session);
-			if (info != null) {
-				MemberEntity member = memberService.getMember(info.getMember()
-						.getId());
-				if (member.getAccountNum() == null) {
-					member.setAccountNum(0);
-				}
-				List<AreaEntity> cityList = areaService.getChildrenAreas("32");
-				map.put("cityList", cityList);
-				map.put("member", member);
-				if (member.getFlag()) {
-					return "site/member/member-profile";
-				} else {
-					return "site/member/edit-member";
-				}
-			}
-			return LOGIN;
-		} catch (Exception e) {
-			SxjLogger.error("修改会员信息错误", e, this.getClass());
-			throw new WebException(e.getMessage());
-		}
-
-	}
-
-	/**
-	 * 修改密码
-	 * 
-	 * @param id
-	 * @param password
-	 * @return
-	 */
-	@RequestMapping("/edit_pwd")
-	public @ResponseBody Map<String, String> edit_pwd(String id, String password) {
-		Map<String, String> map = new HashMap<String, String>();
-		memberService.edit_pwd(id, password);
-		return map;
-	}
-
-	/**
-	 * 修改会员信息界面
-	 * 
-	 * @return
-	 */
-	@RequestMapping("edit_member")
-	public String edit_member(String id, ModelMap map) {
-		MemberEntity member = memberService.getMember(id);
-		List<AreaEntity> cityList = areaService.getChildrenAreas("32");
-		map.put("cityList", cityList);
-		map.put("member", member);
-		return "site/member/edit-member";
-	}
-
-	@RequestMapping("save_member")
-	public @ResponseBody Map<String, Object> save_member(MemberEntity member)
-			throws WebException {
-		try {
-			Map<String, Object> map = new HashMap<String, Object>();
-			member.setFlag(true);
-			memberService.modifyMember(member);
-			map.put("isOK", "ok");
-			return map;
-		} catch (Exception e) {
-			SxjLogger.error("修改会员信息错误", e, this.getClass());
-			throw new WebException(e.getMessage());
-		}
-
-	}
-
-	/**
-	 * 注册账户
-	 */
-	@RequestMapping("/regist")
-	public String regist(ModelMap map) {
-		MemberTypeEnum[] type = MemberTypeEnum.values();
-		List<AreaEntity> list = areaService.getChildrenAreas("32");
-		map.put("type", type);
-		map.put("list", list);
-		return "site/register";
-	}
-
-	/**
-	 * 添加会员
-	 */
-	@RequestMapping("regist_save")
-	public String regist_save(MemberEntity member, ModelMap map, String ms,
-			HttpSession session) {
-		String message = (String) HierarchicalCacheManager.get(2, "checkMs",
-				session.getId() + "_checkMs");
-		if (StringUtils.isEmpty(message)) {
-			MemberTypeEnum[] type = MemberTypeEnum.values();
-			List<AreaEntity> list = areaService.getChildrenAreas("32");
-			map.put("type", type);
-			map.put("list", list);
-			map.put("member", member);
-			return "site/register";
-		}
-		if (message.equals(ms)) {
-			member.setState(MemberStatesEnum.normal);
-			member.setCheckState(MemberCheckStateEnum.unaudited);
-			member.setRegDate(new Date());
-			member.setFlag(false);
-			memberService.addMember(member);
-			return "redirect:/member/regist_succ.htm";
-		} else {
-			MemberTypeEnum[] type = MemberTypeEnum.values();
-			List<AreaEntity> list = areaService.getChildrenAreas("32");
-			map.put("type", type);
-			map.put("list", list);
-			map.put("member", member);
-			return "site/register";
-		}
-	}
-
-	@RequestMapping("regist_succ")
-	public String regist_succ() {
-		return "site/reg-suc";
-	}
-
-	/**
-	 * 忘记密码
-	 */
-	@RequestMapping("find_pwd")
-	public String find_pwd() {
-		return "site/find-password";
-	}
-
-	/**
-	 * 验证会员是否注册过
-	 */
-	@RequestMapping("check_member")
-	public @ResponseBody Map<String, String> check_member(String name) {
-		Map<String, String> map = new HashMap<String, String>();
-		MemberEntity member = memberService.getMemberByName(name);
-		if (member == null) {
-			return null;
-		} else {
-			map.put("text", "该会员已存在！");
-			return map;
-		}
-	}
-
-	/**
-	 * 验证手机号是否已注册
-	 * 
-	 */
-	@RequestMapping("check_phone")
-	public @ResponseBody Map<String, String> check_phone(String phone)
-			throws WebException {
-		Map<String, String> map = new HashMap<String, String>();
-		MemberQuery query = new MemberQuery();
-		query.setContactsPhone(phone);
-		List<MemberEntity> list = memberService.queryMembers(query);
-		if (list.size() > 0) {
-			map.put("flag", "true");
-			map.put("id", list.get(0).getId());
-		} else {
-			map.put("flag", "false");
-		}
-		return map;
-	}
-
-	/**
-	 * 发送验证码
-	 */
-	@RequestMapping("send_ms")
-	public @ResponseBody Map<String, String> send_ms(HttpSession session,
-			String phoneNo) {
-		String message = "123456";
-		message = memberService.createvalidata(phoneNo, message);
-		Map<String, String> map = new HashMap<String, String>();
-		HierarchicalCacheManager.set(2, "checkMs",
-				session.getId() + "_checkMs", message);
-		return map;
-	}
-
-	/**
-	 * 短信验证
-	 */
-	@RequestMapping("check_ms")
-	public @ResponseBody Map<String, String> check_ms(HttpSession session,
-			String ms) throws WebException {
-		Map<String, String> map = new HashMap<String, String>();
-		String message = (String) HierarchicalCacheManager.get(2, "checkMs",
-				session.getId() + "_checkMs");
-		if (StringUtils.isEmpty(message)) {
-			map.put("flag", "false");
-		}
-		if (message.equals(ms)) {
-			map.put("flag", "true");
-		} else {
-			map.put("flag", "false");
-		}
-		return map;
-	}
+public class MemberController extends BaseController
+{
+    
+    @Autowired
+    private IMemberService memberService;
+    
+    @Autowired
+    private IAreaService areaService;
+    
+    /**
+     * 根据会员号获取会员信息
+     * 
+     * @param map
+     * @return
+     * @throws WebException
+     */
+    @RequestMapping("/memberInfo")
+    public String memberList(ModelMap map, HttpSession session,
+            HttpServletRequest request) throws WebException
+    {
+        try
+        {
+            SupervisorPrincipal info = getLoginInfo(session);
+            if (info != null)
+            {
+                MemberEntity member = memberService.getMember(info.getMember()
+                        .getId());
+                if (member.getAccountNum() == null)
+                {
+                    member.setAccountNum(0);
+                }
+                List<AreaEntity> cityList = areaService.getChildrenAreas("32");
+                map.put("cityList", cityList);
+                map.put("member", member);
+                if (member.getFlag())
+                {
+                    return "site/member/member-profile";
+                }
+                else
+                {
+                    return "site/member/edit-member";
+                }
+            }
+            return LOGIN;
+        }
+        catch (Exception e)
+        {
+            SxjLogger.error("修改会员信息错误", e, this.getClass());
+            throw new WebException(e.getMessage());
+        }
+        
+    }
+    
+    /**
+     * 修改密码
+     * 
+     * @param id
+     * @param password
+     * @return
+     */
+    @RequestMapping("/edit_pwd")
+    public @ResponseBody Map<String, String> edit_pwd(String id, String password)
+    {
+        Map<String, String> map = new HashMap<String, String>();
+        memberService.edit_pwd(id, password);
+        return map;
+    }
+    
+    /**
+     * 修改会员信息界面
+     * 
+     * @return
+     */
+    @RequestMapping("edit_member")
+    public String edit_member(String id, ModelMap map)
+    {
+        MemberEntity member = memberService.getMember(id);
+        List<AreaEntity> cityList = areaService.getChildrenAreas("32");
+        map.put("cityList", cityList);
+        map.put("member", member);
+        return "site/member/edit-member";
+    }
+    
+    @RequestMapping("enter")
+    @ResponseBody
+    public void enter(HttpSession session, HttpServletRequest request,
+            String url)
+    {
+        Date enterTime = (Date) session.getAttribute("enterTime");
+        
+        String currentUrl = (String) session.getAttribute("currentUrl");
+        String nextUrl = (String) session.getAttribute("nextUrl");
+        if (currentUrl == null)
+        {
+            session.setAttribute("currentUrl", request.getHeader("Referer"));
+            
+        }
+        session.setAttribute("previousUrl", currentUrl);
+        session.setAttribute("currentUrl", nextUrl);
+        session.setAttribute("nextUrl", url);
+        
+        if (enterTime != null)
+        {
+            MemberEntity principal = (MemberEntity) SecurityUtils.getSubject()
+                    .getPrincipal();
+            System.out.println("Enter At: " + enterTime + "   Leave at: "
+                    + new Date());
+            System.out.println("Entering page: " + principal.getMemberNo()
+                    + "pre:   " + session.getAttribute("previousUrl")
+                    + "------current:    " + session.getAttribute("currentUrl")
+                    + "------next:     " + session.getAttribute("nextUrl"));
+        }
+        session.setAttribute("enterTime", new Date());
+        
+    }
+    
+    @RequestMapping("leave")
+    @ResponseBody
+    public void leave(HttpSession session)
+    {
+        session.setAttribute("leaveTime", new Date());
+        
+    }
+    
+    @RequestMapping("save_member")
+    public @ResponseBody Map<String, Object> save_member(MemberEntity member)
+            throws WebException
+    {
+        try
+        {
+            Map<String, Object> map = new HashMap<String, Object>();
+            member.setFlag(true);
+            memberService.modifyMember(member);
+            map.put("isOK", "ok");
+            return map;
+        }
+        catch (Exception e)
+        {
+            SxjLogger.error("修改会员信息错误", e, this.getClass());
+            throw new WebException(e.getMessage());
+        }
+        
+    }
+    
+    /**
+     * 注册账户
+     */
+    @RequestMapping("/regist")
+    public String regist(ModelMap map)
+    {
+        MemberTypeEnum[] type = MemberTypeEnum.values();
+        List<AreaEntity> list = areaService.getChildrenAreas("32");
+        map.put("type", type);
+        map.put("list", list);
+        return "site/register";
+    }
+    
+    /**
+     * 添加会员
+     */
+    @RequestMapping("regist_save")
+    public String regist_save(MemberEntity member, ModelMap map, String ms,
+            HttpSession session)
+    {
+        String message = (String) HierarchicalCacheManager.get(2,
+                "checkMs",
+                session.getId() + "_checkMs");
+        if (StringUtils.isEmpty(message))
+        {
+            MemberTypeEnum[] type = MemberTypeEnum.values();
+            List<AreaEntity> list = areaService.getChildrenAreas("32");
+            map.put("type", type);
+            map.put("list", list);
+            map.put("member", member);
+            return "site/register";
+        }
+        if (message.equals(ms))
+        {
+            member.setState(MemberStatesEnum.normal);
+            member.setCheckState(MemberCheckStateEnum.unaudited);
+            member.setRegDate(new Date());
+            member.setFlag(false);
+            memberService.addMember(member);
+            return "redirect:/member/regist_succ.htm";
+        }
+        else
+        {
+            MemberTypeEnum[] type = MemberTypeEnum.values();
+            List<AreaEntity> list = areaService.getChildrenAreas("32");
+            map.put("type", type);
+            map.put("list", list);
+            map.put("member", member);
+            return "site/register";
+        }
+    }
+    
+    @RequestMapping("regist_succ")
+    public String regist_succ()
+    {
+        return "site/reg-suc";
+    }
+    
+    /**
+     * 忘记密码
+     */
+    @RequestMapping("find_pwd")
+    public String find_pwd()
+    {
+        return "site/find-password";
+    }
+    
+    /**
+     * 验证会员是否注册过
+     */
+    @RequestMapping("check_member")
+    public @ResponseBody Map<String, String> check_member(String name)
+    {
+        Map<String, String> map = new HashMap<String, String>();
+        MemberEntity member = memberService.getMemberByName(name);
+        if (member == null)
+        {
+            return null;
+        }
+        else
+        {
+            map.put("text", "该会员已存在！");
+            return map;
+        }
+    }
+    
+    /**
+     * 验证手机号是否已注册
+     * 
+     */
+    @RequestMapping("check_phone")
+    public @ResponseBody Map<String, String> check_phone(String phone)
+            throws WebException
+    {
+        Map<String, String> map = new HashMap<String, String>();
+        MemberQuery query = new MemberQuery();
+        query.setContactsPhone(phone);
+        List<MemberEntity> list = memberService.queryMembers(query);
+        if (list.size() > 0)
+        {
+            map.put("flag", "true");
+            map.put("id", list.get(0).getId());
+        }
+        else
+        {
+            map.put("flag", "false");
+        }
+        return map;
+    }
+    
+    /**
+     * 发送验证码
+     */
+    @RequestMapping("send_ms")
+    public @ResponseBody Map<String, String> send_ms(HttpSession session,
+            String phoneNo)
+    {
+        String message = "123456";
+        message = memberService.createvalidata(phoneNo, message);
+        Map<String, String> map = new HashMap<String, String>();
+        HierarchicalCacheManager.set(2,
+                "checkMs",
+                session.getId() + "_checkMs",
+                message);
+        return map;
+    }
+    
+    /**
+     * 短信验证
+     */
+    @RequestMapping("check_ms")
+    public @ResponseBody Map<String, String> check_ms(HttpSession session,
+            String ms) throws WebException
+    {
+        Map<String, String> map = new HashMap<String, String>();
+        String message = (String) HierarchicalCacheManager.get(2,
+                "checkMs",
+                session.getId() + "_checkMs");
+        if (StringUtils.isEmpty(message))
+        {
+            map.put("flag", "false");
+        }
+        if (message.equals(ms))
+        {
+            map.put("flag", "true");
+        }
+        else
+        {
+            map.put("flag", "false");
+        }
+        return map;
+    }
 }
