1bbbaa0f81231f14a7632f80c50b8301db39e231
qjj <qjj@126.com>
2014-10-29 19:31:01 +0800

    me

--- supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/rfid/startmrfid/StartmrfidController.java
+++ supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/rfid/startmrfid/StartmrfidController.java
@@ -1,5 +1,6 @@
 package com.sxj.supervisor.website.controller.rfid.startmrfid;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sxj.supervisor.entity.contract.ContractEntity;
 import com.sxj.supervisor.enu.rfid.window.WindowTypeEnum;
 import com.sxj.supervisor.model.contract.ContractModel;
 import com.sxj.supervisor.model.contract.ContractQuery;
@@ -67,6 +69,30 @@ public class StartmrfidController extends BaseController {
 		} catch (Exception e) {
 			SxjLogger.error("根据合同号查询合同信息错误", e, this.getClass());
 			throw new WebException("根据合同号查询合同信息错误");
+		}
+	}
+
+	/**
+	 * 通过合同号联想查询
+	 */
+	@RequestMapping("lx_query")
+	public @ResponseBody Map<Object, Object> lx_query(String keyword)
+			throws WebException {
+		try {
+			Map<Object, Object> map = new HashMap<Object, Object>();
+			List<ContractEntity> list = contractService
+					.getContractByRefContractNo(keyword);
+			List<Map<String, String>> addlist = new ArrayList<Map<String, String>>();
+			for (ContractEntity contractEntity : list) {
+				Map<String, String> cmap = new HashMap<String, String>();
+				cmap.put("title", contractEntity.getRefContractNo());
+				addlist.add(cmap);
+			}
+			map.put("data", addlist);
+			return map;
+		} catch (Exception e) {
+			SxjLogger.error("联想查询错误", e, this.getClass());
+			throw new WebException("联想查询错误");
 		}
 	}
 }
