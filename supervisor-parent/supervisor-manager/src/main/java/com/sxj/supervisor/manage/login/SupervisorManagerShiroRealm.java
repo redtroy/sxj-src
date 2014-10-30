6909c728bdb1e145d6e4644d1bacc84709de85db
redtroy <redtroy@sxj.com>
2014-10-30 15:34:19 +0800

    Change-Id: Ie44b5eede3a1113cafa3173611dbc9bf496356c5

--- supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/login/SupervisorManagerShiroRealm.java
+++ supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/login/SupervisorManagerShiroRealm.java
@@ -1,7 +1,12 @@
 package com.sxj.supervisor.manage.login;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
@@ -13,9 +18,13 @@ import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.Permission;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.authz.permission.PermissionResolver;
+import org.apache.shiro.authz.permission.RolePermissionResolver;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.sxj.supervisor.entity.system.FunctionEntity;
@@ -130,5 +139,111 @@ public class SupervisorManagerShiroRealm extends AuthorizingRealm
     protected void clearCachedAuthorizationInfo(PrincipalCollection arg0)
     {
         super.clearCachedAuthorizationInfo(arg0);
+    }
+    
+    private boolean isPermitted(Permission permission, AuthorizationInfo info)
+    {
+        Collection<Permission> perms = getPermissions(info);
+        if (perms != null && !perms.isEmpty())
+        {
+            for (Permission perm : perms)
+            {
+                if (perm.implies(permission))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean isPermitted(PrincipalCollection principals,
+            Permission permission)
+    {
+        AuthorizationInfo info = getAuthorizationInfo(principals);
+        return isPermitted(permission, info);
+    }
+    
+    @Override
+    public boolean isPermitted(PrincipalCollection principals, String permission)
+    {
+        Permission p = getPermissionResolver().resolvePermission(permission);
+        return this.isPermitted(principals, p);
+    }
+    
+    private Collection<Permission> resolveRolePermissions(
+            Collection<String> roleNames)
+    {
+        Collection<Permission> perms = Collections.emptySet();
+        RolePermissionResolver resolver = getRolePermissionResolver();
+        if (resolver != null && !CollectionUtils.isEmpty(roleNames))
+        {
+            perms = new LinkedHashSet<Permission>(roleNames.size());
+            for (String roleName : roleNames)
+            {
+                Collection<Permission> resolved = resolver.resolvePermissionsInRole(roleName);
+                if (!CollectionUtils.isEmpty(resolved))
+                {
+                    perms.addAll(resolved);
+                }
+            }
+        }
+        return perms;
+    }
+    
+    private Collection<Permission> resolvePermissions(
+            Collection<String> stringPerms)
+    {
+        Collection<Permission> perms = Collections.emptySet();
+        PermissionResolver resolver = getPermissionResolver();
+        if (resolver != null && !CollectionUtils.isEmpty(stringPerms))
+        {
+            perms = new LinkedHashSet<Permission>(stringPerms.size());
+            for (String strPermission : stringPerms)
+            {
+                Permission permission = getPermissionResolver().resolvePermission(strPermission);
+                perms.add(permission);
+            }
+        }
+        return perms;
+    }
+    
+    private Collection<Permission> getPermissions(AuthorizationInfo info)
+    {
+        Set<Permission> permissions = new HashSet<Permission>();
+        
+        if (info != null)
+        {
+            Collection<Permission> perms = info.getObjectPermissions();
+            if (!CollectionUtils.isEmpty(perms))
+            {
+                permissions.addAll(perms);
+            }
+            
+            //=============================此处检查权限是否变动==============================//
+            
+            //=============================end==========================================//
+            perms = resolvePermissions(info.getStringPermissions());
+            if (!CollectionUtils.isEmpty(perms))
+            {
+                permissions.addAll(perms);
+            }
+            
+            perms = resolveRolePermissions(info.getRoles());
+            if (!CollectionUtils.isEmpty(perms))
+            {
+                permissions.addAll(perms);
+            }
+        }
+        
+        if (permissions.isEmpty())
+        {
+            return Collections.emptySet();
+        }
+        else
+        {
+            return Collections.unmodifiableSet(permissions);
+        }
     }
 }
