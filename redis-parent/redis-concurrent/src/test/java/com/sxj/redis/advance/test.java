cffd259518967446bc894e33702a372a9e6d6a92
redtroy <redtroy@sxj.com>
2014-10-31 14:33:09 +0800

    message

---
+++ redis-parent/redis-concurrent/src/test/java/com/sxj/redis/advance/test.java
@@ -0,0 +1,28 @@
+package com.sxj.redis.advance;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class test
+{
+    
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+    }
+    
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+    
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+    
+}
