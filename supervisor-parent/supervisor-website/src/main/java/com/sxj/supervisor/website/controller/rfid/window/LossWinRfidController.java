73a72a52843c1e71fd4e0509aabfc5c895b395d4
qjj <qjj@126.com>
2014-11-03 19:15:24 +0800

    Change-Id: Icd0b3b3431ef0dac336369a77719551a56dc8368

--- supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/rfid/window/LossWinRfidController.java
+++ supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/rfid/window/LossWinRfidController.java
@@ -1,16 +1,182 @@
 package com.sxj.supervisor.website.controller.rfid.window;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sxj.supervisor.entity.contract.ContractEntity;
+import com.sxj.supervisor.entity.contract.ContractItemEntity;
+import com.sxj.supervisor.entity.rfid.window.WindowRfidEntity;
+import com.sxj.supervisor.enu.rfid.RfidStateEnum;
+import com.sxj.supervisor.enu.rfid.window.WindowTypeEnum;
+import com.sxj.supervisor.model.contract.ContractModel;
+import com.sxj.supervisor.model.contract.ContractQuery;
+import com.sxj.supervisor.model.rfid.window.WindowRfidQuery;
+import com.sxj.supervisor.service.contract.IContractService;
+import com.sxj.supervisor.service.rfid.window.IWindowRfidService;
 import com.sxj.supervisor.website.controller.BaseController;
+import com.sxj.util.exception.WebException;
+import com.sxj.util.logger.SxjLogger;
 
 @Controller
 @RequestMapping("/rfid/window")
 public class LossWinRfidController extends BaseController {
 
+	@Autowired
+	private IWindowRfidService windowRfidService;
+
+	@Autowired
+	private IContractService contractService;
+
 	@RequestMapping("to_loss")
-	public String lossMen() {
+	public String lossMen(ModelMap map) {
+		WindowTypeEnum[] type = WindowTypeEnum.values();
+		map.put("type", type);
 		return "site/rfid/window/lossrfid";
+	}
+
+	@RequestMapping("queryRefContract")
+	public @ResponseBody Map<Object, Object> query(ContractQuery query, Long num)
+			throws WebException {
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		try {
+			List<ContractItemEntity> items = contractService
+					.getContractItem(query.getRefContractNo());
+			if (items == null || items.size() == 0) {
+				throw new WebException("招标合同不存在");
+			}
+			float quantity = 0f;
+			float hasStartQuantity = 0f;
+			for (ContractItemEntity item : items) {
+				if (item == null) {
+					continue;
+				}
+				quantity = quantity + item.getQuantity();
+			}
+
+			WindowRfidQuery winQuery = new WindowRfidQuery();
+			winQuery.setContractNo(query.getRefContractNo());
+			winQuery.setRfidState(RfidStateEnum.used.getId());
+			List<WindowRfidEntity> winList = windowRfidService
+					.queryWindowRfid(winQuery);
+			if (winList != null) {
+				hasStartQuantity = winList.size();
+			}
+			if (hasStartQuantity >= quantity) {
+				throw new WebException("此招标合同已经全部启用完毕");
+			}
+			if (num > (quantity - hasStartQuantity)) {
+				throw new WebException("此招标合同未启用数量为："
+						+ (quantity - hasStartQuantity));
+			}
+
+			List<ContractModel> list = contractService.queryContracts(query);
+			if (list.size() > 0) {
+				String[] bq = windowRfidService.getMaxRfidNo(
+						query.getRefContractNo(), num);
+				map.put("isOk", "ok");
+				map.put("bq", bq);
+				map.put("list", list);
+			} else {
+				map.put("isOk", "false");
+			}
+			return map;
+		} catch (Exception e) {
+			SxjLogger.error(e.getMessage(), e, this.getClass());
+			map.put("error", e.getMessage());
+		}
+		return map;
+	}
+
+	@RequestMapping("query_contractNo")
+	public @ResponseBody Map<Object, Object> query_contractNo(String contractNo)
+			throws WebException {
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		try {
+			ContractModel cm = contractService
+					.getContractModelByContractNo(contractNo);
+			if (cm != null) {
+				map.put("isOk", "ok");
+				map.put("cm", cm);
+			} else {
+				map.put("isOk", "false");
+			}
+
+		} catch (Exception e) {
+			SxjLogger.error("根据合同号查询合同信息错误", e, this.getClass());
+			map.put("error", e.getMessage());
+		}
+		return map;
+	}
+
+	/**
+	 * 通过合同号联想查询
+	 */
+	@RequestMapping("lx_query")
+	public @ResponseBody Map<Object, Object> lx_query(String keyword)
+			throws WebException {
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		try {
+			List<ContractEntity> list = contractService
+					.getContractByRefContractNo(keyword);
+			List<Map<String, String>> addlist = new ArrayList<Map<String, String>>();
+			for (ContractEntity contractEntity : list) {
+				Map<String, String> cmap = new HashMap<String, String>();
+				cmap.put("title", contractEntity.getRefContractNo());
+				addlist.add(cmap);
+			}
+			map.put("data", addlist);
+
+		} catch (Exception e) {
+			SxjLogger.error("联想查询错误", e, this.getClass());
+			map.put("error", e.getMessage());
+		}
+		return map;
+	}
+
+	/**
+	 * 启用标签
+	 */
+	@RequestMapping("start_lable")
+	public @ResponseBody Map<Object, Object> start_lable(String refContractNo,
+			String minRfid, String maxRfid, String gRfid, String lRfid,
+			WindowTypeEnum windowType) throws WebException {
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		try {
+			ContractModel refContract = contractService
+					.getContractModelByContractNo(refContractNo);
+			if (refContract == null) {
+				throw new WebException("招标合同不存在");
+			}
+			List<ContractItemEntity> items = refContract.getItemList();
+			if (items == null || items.size() == 0) {
+				throw new WebException("招标合同条目不存在");
+			}
+			float quantity = 0f;
+			for (Iterator<ContractItemEntity> iterator = items.iterator(); iterator
+					.hasNext();) {
+				ContractItemEntity item = iterator.next();
+				if (item == null) {
+					continue;
+				}
+				quantity = quantity + item.getQuantity();
+			}
+			long count = (long) quantity;
+			windowRfidService.startWindowRfid(count, refContractNo, minRfid,
+					maxRfid, gRfid, lRfid, windowType);
+			map.put("isOk", "ok");
+		} catch (Exception e) {
+			SxjLogger.error("启用标签错误", e, this.getClass());
+			map.put("error", e.getMessage());
+		}
+		return map;
 	}
 }
