22855a6f0c9b23a4328c5ef2c52e587e8b627245
Ann <Ann@An-PC>
2014-11-05 16:46:19 +0800

    message

---
+++ sxj-common/src/main/java/com/sxj/util/Constraints.java
@@ -0,0 +1,12 @@
+package com.sxj.util;
+
+public interface Constraints
+{
+    public static final String WEBSITE = "website";
+    
+    public static final String MANAGER = "manager";
+    
+    public static final String SHIRO_CHANNEL_NAME = "shiro-channel";
+    
+    public static final String SHIRO_MAP_KEY = "shiro_map";
+}
