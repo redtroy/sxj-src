151c1fa22f6f58ab509556a0c2a69b39e19dd8ac
Ann <Ann@An-PC>
2014-11-05 17:11:38 +0800

    Change-Id: I985c7ce319917e92420dc0831162a676cd2865a5

--- supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/login/SupervisorManagerMessageListener.java
+++ supervisor-parent/supervisor-manager/src/main/java/com/sxj/supervisor/manage/login/SupervisorManagerMessageListener.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.sxj.supervisor.manage.login;
 
 import com.sxj.redis.advance.core.MessageListener;
@@ -33,39 +32,3 @@ public class SupervisorManagerMessageListener
     }
     
 }
-=======
-package com.sxj.supervisor.manage.login;
-
-import com.sxj.redis.advance.core.MessageListener;
-import com.sxj.redis.advance.topic.RedisTopics;
-
-public class SupervisorManagerMessageListener
-{
-    private SupervisorManagerShiroRealm realm;
-    
-    private RedisTopics topics;
-    
-    public SupervisorManagerMessageListener()
-    {
-        topics.getTopic("").addListener(new MessageListener<Object>()
-        {
-            @Override
-            public void onMessage(Object msg)
-            {
-                System.out.println(msg);
-            }
-        });
-    }
-    
-    public void setRealm(SupervisorManagerShiroRealm realm)
-    {
-        this.realm = realm;
-    }
-    
-    public void setTopics(RedisTopics topics)
-    {
-        this.topics = topics;
-    }
-    
-}
->>>>>>> cb7e17a58b6eb6b575ffea075e60859951e1c03e
