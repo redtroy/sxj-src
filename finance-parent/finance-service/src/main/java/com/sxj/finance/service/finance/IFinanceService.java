86ae76ac2ddc852be64e1f7e50d53db50c5ad4e4
redtroy <redtroy@sxj.com>
2014-12-01 15:30:33 +0800

    me

--- finance-parent/finance-service/src/main/java/com/sxj/finance/service/finance/IFinanceService.java
+++ finance-parent/finance-service/src/main/java/com/sxj/finance/service/finance/IFinanceService.java
@@ -1,5 +1,14 @@
 package com.sxj.finance.service.finance;
 
+import java.util.List;
+
+import com.sxj.finance.entity.FinanceEntity;
+import com.sxj.finance.model.finance.FinanceModel;
+import com.sxj.util.exception.ServiceException;
+
 public interface IFinanceService {
+
+	public List<FinanceEntity> query(FinanceModel query)
+			throws ServiceException;
 
 }
