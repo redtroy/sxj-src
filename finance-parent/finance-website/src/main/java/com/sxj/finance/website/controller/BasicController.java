df840c130bf0322dcd9923d9800583ea1a6c23d9
djx <djx@sxj.com>
2014-12-02 15:47:45 +0800

    Change-Id: Ib1a651ef91d3a26e648d06fb338f7f30767f7ef5

--- finance-parent/finance-website/src/main/java/com/sxj/finance/website/controller/BasicController.java
+++ finance-parent/finance-website/src/main/java/com/sxj/finance/website/controller/BasicController.java
@@ -1,15 +1,56 @@
 package com.sxj.finance.website.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sxj.cache.manager.HierarchicalCacheManager;
+import com.sxj.finance.entity.member.MemberEntity;
+import com.sxj.finance.service.member.IMemberService;
+import com.sxj.finance.website.login.SupervisorPrincipal;
+import com.sxj.finance.website.login.SupervisorSiteToken;
 import com.sxj.util.LoginToken;
+import com.sxj.util.common.EncryptUtil;
+import com.sxj.util.common.StringUtils;
+import com.sxj.util.logger.SxjLogger;
 
 @Controller
 public class BasicController extends BaseController {
+
+	@Autowired
+	private IMemberService memberService;
+
+	@RequestMapping("index")
+	public String ToIndex(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session == null || session.getAttribute("userinfo") == null) {
+			return LOGIN;
+		} else {
+			SupervisorPrincipal info = getLoginInfo(session);
+			if (info.getMember() != null) {
+				String function = request.getParameter("function");
+				if (StringUtils.isNotEmpty(function)) {
+					return "redirect:" + getBasePath(request)
+							+ "member/memberInfo.htm?function=" + function;
+				} else {
+					return "redirect:" + getBasePath(request)
+							+ "member/memberInfo.htm";
+				}
+
+			} else {
+				return LOGIN;
+			}
+
+		}
+
+	}
 
 	@RequestMapping("to_login")
 	public String ToLogin() {
@@ -17,15 +58,46 @@ public class BasicController extends BaseController {
 	}
 
 	@RequestMapping("/autoLogin")
-	public String autoLogin(String member, String token,
-			HttpServletRequest request) {
+	public String autoLogin(String member, String token, HttpSession session,
+			HttpServletRequest request, ModelMap map) {
 		try {
 			String retUrl = request.getHeader("Referer");
-			retUrl.toCharArray();
+			MemberEntity memberInfo = memberService.memberInfo(member);
+			if (memberInfo == null) {
+				return LOGIN;
+			}
+			LoginToken newToken = new LoginToken();
+			newToken.setMemberNo(memberInfo.getMemberNo());
+			newToken.setMemberName(memberInfo.getName());
+			newToken.setPassword(memberInfo.getPassword());
+			String tokenMd5 = EncryptUtil.md5Hex(newToken.toString());
+			if (!tokenMd5.equals(token)) {
+				return LOGIN;
+			}
+			SupervisorPrincipal userBean = new SupervisorPrincipal();
+			userBean.setMember(memberInfo);
+			SupervisorSiteToken siteToken = new SupervisorSiteToken(userBean,
+					memberInfo.getPassword());
+			Subject currentUser = SecurityUtils.getSubject();
+			try {
+				currentUser.login(siteToken);
+			} catch (AuthenticationException e) {
+				SxjLogger.error("登陆失败", e, this.getClass());
+				map.put("pmessage", "密码错误");
+				return LOGIN;
+
+			}
+			if (currentUser.isAuthenticated()) {
+				session.setAttribute("userinfo", userBean);
+				return "redirect:" + getBasePath(request) + "index.htm";
+			} else {
+				map.put("message", "登陆失败");
+				return LOGIN;
+			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			map.put("message", "登陆失败");
+			return LOGIN;
 		}
-		return null;
 	}
 
 	@RequestMapping("/login")
---
+++ finance-parent/finance-website/src/main/java/com/sxj/finance/website/login/SupervisorPrincipal.java
@@ -0,0 +1,35 @@
+package com.sxj.finance.website.login;
+
+import java.io.Serializable;
+
+import com.sxj.finance.entity.member.AccountEntity;
+import com.sxj.finance.entity.member.MemberEntity;
+
+public class SupervisorPrincipal implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7928061127084410598L;
+
+	private MemberEntity member;
+
+	private AccountEntity account;
+
+	public MemberEntity getMember() {
+		return member;
+	}
+
+	public void setMember(MemberEntity member) {
+		this.member = member;
+	}
+
+	public AccountEntity getAccount() {
+		return account;
+	}
+
+	public void setAccount(AccountEntity account) {
+		this.account = account;
+	}
+
+}
