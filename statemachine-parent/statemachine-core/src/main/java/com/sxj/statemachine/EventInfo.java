828a728b88382974d919d828f3e5925eb163009f
redtroy <redtroy@sxj.com>
2014-12-26 13:30:36 +0800

    Change-Id: I48bcaac1bc134770175bde9acb1d94f300c91cfd

--- statemachine-parent/statemachine-core/src/main/java/com/sxj/statemachine/EventInfo.java
+++ statemachine-parent/statemachine-core/src/main/java/com/sxj/statemachine/EventInfo.java
@@ -1,18 +1,3 @@
-/*  
- * Copyright 2012 xavi.ferro
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */   
 package com.sxj.statemachine;
 
 import com.sxj.statemachine.interfaces.EnterStateController;
@@ -28,11 +13,14 @@ import com.sxj.statemachine.interfaces.EnterStateController;
  * <p>This  is quite useful for some conditional states that might evaluate in runtime next event to be
  * consumed.
  */
-public class EventInfo {
+public class EventInfo
+{
     protected String event;
+    
     protected Object object;
     
-    public EventInfo(String event, Object object) {
+    public EventInfo(String event, Object object)
+    {
         this.event = event;
         this.object = object;
     }
@@ -40,7 +28,8 @@ public class EventInfo {
     /**
      * The event we want to consume after the phase is finished
      */
-    public String getEvent() {
+    public String getEvent()
+    {
         return this.event;
     }
     
@@ -48,7 +37,8 @@ public class EventInfo {
      * The object that we passed to the previous transition
      * @return
      */
-    public Object getObject() {
+    public Object getObject()
+    {
         return this.object;
     }
 }
