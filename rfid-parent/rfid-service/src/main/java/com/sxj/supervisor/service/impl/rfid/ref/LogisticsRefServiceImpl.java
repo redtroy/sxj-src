5d1360df1eaf0bf6b3323479275cdffc55dd5cf5
qjj[merge] <qjj@sxj.com>
2015-01-16 15:24:24 +0800

    Signed-off-by: qjj[merge] <qjj@sxj.com>

--- rfid-parent/rfid-service/src/main/java/com/sxj/supervisor/service/impl/rfid/ref/LogisticsRefServiceImpl.java
+++ rfid-parent/rfid-service/src/main/java/com/sxj/supervisor/service/impl/rfid/ref/LogisticsRefServiceImpl.java
@@ -11,7 +11,6 @@ import com.sxj.supervisor.dao.rfid.ref.ILogisticsRefDao;
 import com.sxj.supervisor.entity.rfid.ref.LogisticsRefEntity;
 import com.sxj.supervisor.model.comet.RfidChannel;
 import com.sxj.supervisor.model.rfid.ref.LogisticsRefQuery;
-import com.sxj.supervisor.service.rfid.logistics.ILogisticsRfidService;
 import com.sxj.supervisor.service.rfid.ref.ILogisticsRefService;
 import com.sxj.util.common.DateTimeUtils;
 import com.sxj.util.exception.ServiceException;
@@ -19,124 +18,100 @@ import com.sxj.util.logger.SxjLogger;
 import com.sxj.util.persistent.QueryCondition;
 
 @Service
-public class LogisticsRefServiceImpl implements ILogisticsRefService
-{
-    
-    @Autowired
-    private ILogisticsRefDao refDao;
-    
-    @Autowired
-    private ILogisticsRfidService rfidService;
-    
-    @Override
-    @Transactional(readOnly = true)
-    public List<LogisticsRefEntity> query(LogisticsRefQuery query)
-            throws ServiceException
-    {
-        try
-        {
-            QueryCondition<LogisticsRefEntity> condition = new QueryCondition<LogisticsRefEntity>();
-            if (query != null)
-            {
-                condition.addCondition("rfidRefNo", query.getRfidRefNo());
-                condition.addCondition("rfidNo", query.getRfidNo());
-                condition.addCondition("memberName", query.getMemberName());
-                condition.addCondition("contractNo", query.getContractNo());
-                condition.addCondition("replenishRfid",
-                        query.getReplenishRfid());
-                condition.addCondition("rfidType", query.getRfidType());
-                condition.addCondition("type", query.getType());
-                condition.addCondition("state", query.getState());
-                condition.addCondition("starApplyDate",
-                        query.getStarApplyDate());// RFID类型
-                condition.addCondition("endApplyDate", query.getEndApplyDate());// RFID类型
-                condition.addCondition("delstate", query.getDelstate());
-                condition.setPage(query);
-            }
-            List<LogisticsRefEntity> list = refDao.queryList(condition);
-            query.setPage(condition);
-            return list;
-        }
-        catch (Exception e)
-        {
-            SxjLogger.error(e.getMessage(), e, this.getClass());
-            throw new ServiceException("物流RFID关联申请管理查询错误");
-        }
-    }
-    
-    @Override
-    @Transactional
-    public void update(LogisticsRefEntity model) throws ServiceException
-    {
-        try
-        {
-            refDao.update(model);
-            CometServiceImpl.subCount(RfidChannel.RFID_MANAGER_LOGISTICS_MESSGAGE_REF);
-            RfidChannel.initTopic()
-                    .publish(RfidChannel.RFID_MANAGER_LOGISTICS_MESSGAGE_REF);
-        }
-        catch (Exception e)
-        {
-            SxjLogger.error(e.getMessage(), e, this.getClass());
-            throw new ServiceException("物流RFID关联申请管理更新错误");
-        }
-        
-    }
-    
-    /**
-     * 删除
-     */
-    @Override
-    @Transactional
-    public void del(String id) throws ServiceException
-    {
-        try
-        {
-            refDao.delete(id);
-        }
-        catch (Exception e)
-        {
-            SxjLogger.error(e.getMessage(), e, this.getClass());
-            throw new ServiceException("物流RFID关联申请管理删除错误");
-        }
-        
-    }
-    
-    @Override
-    @Transactional
-    public void add(LogisticsRefEntity model) throws ServiceException
-    {
-        try
-        {
-            System.out.println(Thread.currentThread());
-            if (model != null)
-            {
-                model.setDateNo("GL" + DateTimeUtils.getTime("yyMM"));
-                refDao.add(model);
-                CometServiceImpl.takeCount(RfidChannel.RFID_MANAGER_LOGISTICS_MESSGAGE_REF);
-                RfidChannel.initTopic()
-                        .publish(RfidChannel.RFID_MANAGER_LOGISTICS_MESSGAGE_REF);
-            }
-        }
-        catch (Exception e)
-        {
-            SxjLogger.error(e.getMessage(), e, LogisticsRefServiceImpl.class);
-            throw new ServiceException();
-        }
-    }
-    
-    @Override
-    public LogisticsRefEntity getRef(String id) throws ServiceException
-    {
-        try
-        {
-            return refDao.get(id);
-        }
-        catch (Exception e)
-        {
-            SxjLogger.error(e.getMessage(), e, this.getClass());
-            throw new ServiceException("获取物流RFID关联申请错误");
-        }
-    }
-    
+public class LogisticsRefServiceImpl implements ILogisticsRefService {
+
+	@Autowired
+	private ILogisticsRefDao refDao;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<LogisticsRefEntity> query(LogisticsRefQuery query)
+			throws ServiceException {
+		try {
+			QueryCondition<LogisticsRefEntity> condition = new QueryCondition<LogisticsRefEntity>();
+			if (query != null) {
+				condition.addCondition("rfidRefNo", query.getRfidRefNo());
+				condition.addCondition("rfidNo", query.getRfidNo());
+				condition.addCondition("memberName", query.getMemberName());
+				condition.addCondition("contractNo", query.getContractNo());
+				condition.addCondition("replenishRfid",
+						query.getReplenishRfid());
+				condition.addCondition("rfidType", query.getRfidType());
+				condition.addCondition("type", query.getType());
+				condition.addCondition("state", query.getState());
+				condition.addCondition("starApplyDate",
+						query.getStarApplyDate());// RFID类型
+				condition.addCondition("endApplyDate", query.getEndApplyDate());// RFID类型
+				condition.addCondition("delstate", query.getDelstate());
+				condition.setPage(query);
+			}
+			List<LogisticsRefEntity> list = refDao.queryList(condition);
+			query.setPage(condition);
+			return list;
+		} catch (Exception e) {
+			SxjLogger.error(e.getMessage(), e, this.getClass());
+			throw new ServiceException("物流RFID关联申请管理查询错误");
+		}
+	}
+
+	@Override
+	@Transactional
+	public void update(LogisticsRefEntity model) throws ServiceException {
+		try {
+			refDao.update(model);
+			CometServiceImpl
+					.subCount(RfidChannel.RFID_MANAGER_LOGISTICS_MESSGAGE_REF);
+			RfidChannel.initTopic().publish(
+					RfidChannel.RFID_MANAGER_LOGISTICS_MESSGAGE_REF);
+		} catch (Exception e) {
+			SxjLogger.error(e.getMessage(), e, this.getClass());
+			throw new ServiceException("物流RFID关联申请管理更新错误");
+		}
+
+	}
+
+	/**
+	 * 删除
+	 */
+	@Override
+	@Transactional
+	public void del(String id) throws ServiceException {
+		try {
+			refDao.delete(id);
+		} catch (Exception e) {
+			SxjLogger.error(e.getMessage(), e, this.getClass());
+			throw new ServiceException("物流RFID关联申请管理删除错误");
+		}
+
+	}
+
+	@Override
+	@Transactional
+	public void add(LogisticsRefEntity model) throws ServiceException {
+		try {
+			System.out.println(Thread.currentThread());
+			if (model != null) {
+				model.setDateNo("GL" + DateTimeUtils.getTime("yyMM"));
+				refDao.add(model);
+				CometServiceImpl
+						.takeCount(RfidChannel.RFID_MANAGER_LOGISTICS_MESSGAGE_REF);
+				RfidChannel.initTopic().publish(
+						RfidChannel.RFID_MANAGER_LOGISTICS_MESSGAGE_REF);
+			}
+		} catch (Exception e) {
+			SxjLogger.error(e.getMessage(), e, LogisticsRefServiceImpl.class);
+			throw new ServiceException();
+		}
+	}
+
+	@Override
+	public LogisticsRefEntity getRef(String id) throws ServiceException {
+		try {
+			return refDao.get(id);
+		} catch (Exception e) {
+			SxjLogger.error(e.getMessage(), e, this.getClass());
+			throw new ServiceException("获取物流RFID关联申请错误");
+		}
+	}
+
 }
