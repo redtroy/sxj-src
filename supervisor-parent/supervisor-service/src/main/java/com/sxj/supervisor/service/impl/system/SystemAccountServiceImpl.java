e543307f689756183ff9b0d9a149d0968dd91e28
djx <djx@sxj.com>
2015-01-21 12:44:14 +0800

    Change-Id: I4c5148575c9a9a77b2ed0983c1ed4035f6d77c5a

--- supervisor-parent/supervisor-service/src/main/java/com/sxj/supervisor/service/impl/system/SystemAccountServiceImpl.java
+++ supervisor-parent/supervisor-service/src/main/java/com/sxj/supervisor/service/impl/system/SystemAccountServiceImpl.java
@@ -27,289 +27,236 @@ import com.sxj.util.persistent.ResultListImpl;
 
 @Service
 @Transactional
-public class SystemAccountServiceImpl implements ISystemAccountService
-{
-    
-    @Autowired
-    private ISystemAccountDao accountDao;
-    
-    @Autowired
-    private IRoleService roleServce;
-    
-    @Override
-    @Transactional
-    public void addAccount(SystemAccountEntity account, String[] functionIds)
-            throws ServiceException
-    {
-        try
-        {
-            Assert.hasText(account.getPassword());
-            SystemAccountEntity oldAccount = getAccountByAccount(account.getAccount());
-            Assert.notNull(oldAccount, "用户账户已存在");
-            //            if (oldAccount != null)
-            //            {
-            //                throw new ServiceException("用户账户已存在");
-            //            }
-            account.setPassword(EncryptUtil.md5Hex(account.getPassword()));
-            accountDao.addSystemAccount(account);
-            if (functionIds != null && functionIds.length > 0)
-            {
-                List<RoleEntity> roles = new ArrayList<RoleEntity>();
-                for (int i = 0; i < functionIds.length; i++)
-                {
-                    //                    if (functionIds[i] == null)
-                    //                    {
-                    //                        continue;
-                    //                    }
-                    //                    if ("none".equals(functionIds[i]))
-                    //                    {
-                    //                        continue;
-                    //                    }
-                    if (functionIds[i] != null
-                            && !"none".equals(functionIds[i]))
-                    {
-                        RoleEntity role = new RoleEntity();
-                        role.setAccountId(account.getId());
-                        role.setFunctionId(functionIds[i]);
-                        roles.add(role);
-                    }
-                }
-                Assert.notEmpty(roles);
-                roleServce.addRoles(roles);
-            }
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException(e.getMessage(), e);
-        }
-        
-    }
-    
-    @Override
-    @Transactional
-    public void modifyAccount(SystemAccountEntity account, String[] functionIds)
-            throws ServiceException
-    {
-        try
-        {
-            Assert.notNull(account);
-            Assert.hasText(account.getId());
-            SystemAccountEntity systemAccount = accountDao.getSystemAccount(account.getId());
-            Assert.notNull(systemAccount);
-            //            if (account == null)
-            //            {
-            //                return;
-            //            }
-            if (functionIds != null && functionIds.length > 0)
-            {
-                List<RoleEntity> roles = new ArrayList<RoleEntity>();
-                for (int i = 0; i < functionIds.length; i++)
-                {
-                    //                    if (functionIds[i] == null)
-                    //                    {
-                    //                        continue;
-                    //                    }
-                    //                    if ("none".equals(functionIds[i]))
-                    //                    {
-                    //                        continue;
-                    //                    }
-                    if (functionIds[i] != null
-                            && !"none".equals(functionIds[i]))
-                    {
-                        RoleEntity role = new RoleEntity();
-                        role.setAccountId(systemAccount.getId());
-                        role.setFunctionId(functionIds[i]);
-                        roles.add(role);
-                    }
-                }
-                if (!CollectionUtils.isEmpty(roles))
-                {
-                    roleServce.removeRoles(systemAccount.getId());
-                    roleServce.addRoles(roles);
-                }
-            }
-            else
-            {
-                roleServce.removeRoles(systemAccount.getId());
-            }
-            if (StringUtils.isNotEmpty(account.getPassword()))
-            {
-                systemAccount.setPassword(EncryptUtil.md5Hex(account.getPassword()));
-            }
-            accountDao.updateSystemAccount(systemAccount);
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException("修改系统用户信息错误", e);
-        }
-        
-    }
-    
-    @Override
-    @Transactional
-    public void deleteAccount(String id) throws ServiceException
-    {
-        try
-        {
-            accountDao.deleteSystemAccount(id);
-            roleServce.removeRoles(id);
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException("删除系统用户信息错误", e);
-        }
-        
-    }
-    
-    @Override
-    public SystemAccountEntity getAccount(String id) throws ServiceException
-    {
-        try
-        {
-            return accountDao.getSystemAccount(id);
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException("查询系统用户信息错误", e);
-        }
-    }
-    
-    @Override
-    @Transactional(readOnly = true)
-    public SystemAccountEntity getAccountByAccount(String account)
-            throws ServiceException
-    {
-        try
-        {
-            if (StringUtils.isEmpty(account))
-            {
-                return null;
-            }
-            QueryCondition<SystemAccountEntity> condition = new QueryCondition<SystemAccountEntity>();
-            condition.addCondition("account", account);
-            List<SystemAccountEntity> list = accountDao.querySystemAccount(condition);
-            if (list != null && list.size() > 0)
-            {
-                return list.get(0);
-            }
-            return null;
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException("查询系统用户信息失败", e);
-        }
-    }
-    
-    @Override
-    public SystemAccountEntity getAccountByAccountNo(String accountNo)
-            throws ServiceException
-    {
-        try
-        {
-            if (StringUtils.isEmpty(accountNo))
-            {
-                return null;
-            }
-            QueryCondition<SystemAccountEntity> condition = new QueryCondition<SystemAccountEntity>();
-            condition.addCondition("accountNo", accountNo);
-            List<SystemAccountEntity> list = accountDao.querySystemAccount(condition);
-            if (list != null && list.size() > 0)
-            {
-                return list.get(0);
-            }
-            return null;
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException("查询系统用户信息失败", e);
-        }
-    }
-    
-    @Override
-    public ResultList<SystemAccountEntity> queryAccounts(SysAccountQuery query)
-            throws ServiceException
-    {
-        try
-        {
-            QueryCondition<SystemAccountEntity> condition = new QueryCondition<SystemAccountEntity>();
-            if (query != null)
-            {
-                condition.addCondition("id", query.getId());
-                condition.addCondition("accountNo", query.getAccountNo());
-                condition.addCondition("name", query.getName());
-                condition.addCondition("account", query.getAccount());
-                condition.addCondition("functionId", query.getFunctionId());
-                condition.setPage(query);
-            }
-            List<SystemAccountEntity> list = accountDao.querySystemAccount(condition);
-            query.setPage(condition);
-            ResultList<SystemAccountEntity> res = new ResultListImpl<SystemAccountEntity>();
-            res.setResults(list);
-            return res;
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException("查询系统用户错误", e);
-        }
-    }
-    
-    @Override
-    @Transactional
-    public String initPassword(String accountId) throws ServiceException
-    {
-        try
-        {
-            int rondom = NumberUtils.getRandomIntInMax(999999);
-            String password = StringUtils.getLengthStr(rondom + "", 6, '0');
-            SystemAccountEntity account = new SystemAccountEntity();
-            account.setId(accountId);
-            account.setPassword(EncryptUtil.md5Hex(password));
-            accountDao.updateSystemAccount(account);
-            return password;
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException("初始化系统用户密码错误", e);
-        }
-    }
-    
-    @Override
-    public void updateLoginTime(String accountId) throws ServiceException
-    {
-        try
-        {
-            SystemAccountEntity account = new SystemAccountEntity();
-            account.setId(accountId);
-            account.setLastLogin(new Date());
-            accountDao.updateSystemAccount(account);
-        }
-        catch (Exception e)
-        {
-            throw new ServiceException("更新系統用戶登錄時間错误", e);
-        }
-        
-    }
-    
-    @Override
-    public String edit_pwd(String id, String password) throws ServiceException
-    {
-        SystemAccountEntity account = new SystemAccountEntity();
-        account.setId(id);
-        account.setPassword(password);
-        try
-        {
-            if (StringUtils.isNotEmpty(account.getPassword()))
-            {
-                account.setPassword(EncryptUtil.md5Hex(account.getPassword()));
-            }
-            accountDao.updateSystemAccount(account);
-        }
-        catch (SQLException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return null;
-    }
-    
+public class SystemAccountServiceImpl implements ISystemAccountService {
+
+	@Autowired
+	private ISystemAccountDao accountDao;
+
+	@Autowired
+	private IRoleService roleServce;
+
+	@Override
+	@Transactional
+	public void addAccount(SystemAccountEntity account, String[] functionIds)
+			throws ServiceException {
+		try {
+			Assert.hasText(account.getPassword());
+			SystemAccountEntity oldAccount = getAccountByAccount(account
+					.getAccount());
+			Assert.notNull(oldAccount, "用户账户已存在");
+			// if (oldAccount != null)
+			// {
+			// throw new ServiceException("用户账户已存在");
+			// }
+			account.setPassword(EncryptUtil.md5Hex(account.getPassword()));
+			accountDao.addSystemAccount(account);
+			if (functionIds != null && functionIds.length > 0) {
+				List<RoleEntity> roles = new ArrayList<RoleEntity>();
+				for (int i = 0; i < functionIds.length; i++) {
+					// if (functionIds[i] == null)
+					// {
+					// continue;
+					// }
+					// if ("none".equals(functionIds[i]))
+					// {
+					// continue;
+					// }
+					if (functionIds[i] != null
+							&& !"none".equals(functionIds[i])) {
+						RoleEntity role = new RoleEntity();
+						role.setAccountId(account.getId());
+						role.setFunctionId(functionIds[i]);
+						roles.add(role);
+					}
+				}
+				Assert.notEmpty(roles);
+				roleServce.addRoles(roles);
+			}
+		} catch (Exception e) {
+			throw new ServiceException(e.getMessage(), e);
+		}
+
+	}
+
+	@Override
+	@Transactional
+	public void modifyAccount(SystemAccountEntity account, String[] functionIds)
+			throws ServiceException {
+		try {
+			Assert.notNull(account);
+			Assert.hasText(account.getId());
+			SystemAccountEntity systemAccount = accountDao
+					.getSystemAccount(account.getId());
+			Assert.notNull(systemAccount);
+			// if (account == null)
+			// {
+			// return;
+			// }
+			if (functionIds != null && functionIds.length > 0) {
+				List<RoleEntity> roles = new ArrayList<RoleEntity>();
+				for (int i = 0; i < functionIds.length; i++) {
+					// if (functionIds[i] == null)
+					// {
+					// continue;
+					// }
+					// if ("none".equals(functionIds[i]))
+					// {
+					// continue;
+					// }
+					if (functionIds[i] != null
+							&& !"none".equals(functionIds[i])) {
+						RoleEntity role = new RoleEntity();
+						role.setAccountId(systemAccount.getId());
+						role.setFunctionId(functionIds[i]);
+						roles.add(role);
+					}
+				}
+				if (!CollectionUtils.isEmpty(roles)) {
+					roleServce.removeRoles(systemAccount.getId());
+					roleServce.addRoles(roles);
+				}
+			} else {
+				roleServce.removeRoles(systemAccount.getId());
+			}
+			if (StringUtils.isNotEmpty(account.getPassword())) {
+				systemAccount.setPassword(EncryptUtil.md5Hex(account
+						.getPassword()));
+			}
+			accountDao.updateSystemAccount(systemAccount);
+		} catch (Exception e) {
+			throw new ServiceException("修改系统用户信息错误", e);
+		}
+
+	}
+
+	@Override
+	@Transactional
+	public void deleteAccount(String id) throws ServiceException {
+		try {
+			accountDao.deleteSystemAccount(id);
+			roleServce.removeRoles(id);
+		} catch (Exception e) {
+			throw new ServiceException("删除系统用户信息错误", e);
+		}
+
+	}
+
+	@Override
+	public SystemAccountEntity getAccount(String id) throws ServiceException {
+		try {
+			return accountDao.getSystemAccount(id);
+		} catch (Exception e) {
+			throw new ServiceException("查询系统用户信息错误", e);
+		}
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public SystemAccountEntity getAccountByAccount(String account)
+			throws ServiceException {
+		try {
+			if (StringUtils.isEmpty(account)) {
+				return null;
+			}
+			QueryCondition<SystemAccountEntity> condition = new QueryCondition<SystemAccountEntity>();
+			condition.addCondition("account", account);
+			List<SystemAccountEntity> list = accountDao
+					.querySystemAccount(condition);
+			if (list != null && list.size() > 0) {
+				return list.get(0);
+			}
+			return null;
+		} catch (Exception e) {
+			throw new ServiceException("查询系统用户信息失败", e);
+		}
+	}
+
+	@Override
+	public SystemAccountEntity getAccountByAccountNo(String accountNo)
+			throws ServiceException {
+		try {
+			if (StringUtils.isEmpty(accountNo)) {
+				return null;
+			}
+			QueryCondition<SystemAccountEntity> condition = new QueryCondition<SystemAccountEntity>();
+			condition.addCondition("accountNo", accountNo);
+			List<SystemAccountEntity> list = accountDao
+					.querySystemAccount(condition);
+			if (list != null && list.size() > 0) {
+				return list.get(0);
+			}
+			return null;
+		} catch (Exception e) {
+			throw new ServiceException("查询系统用户信息失败", e);
+		}
+	}
+
+	@Override
+	public ResultList<SystemAccountEntity> queryAccounts(SysAccountQuery query)
+			throws ServiceException {
+		try {
+			QueryCondition<SystemAccountEntity> condition = new QueryCondition<SystemAccountEntity>();
+			if (query != null) {
+				condition.addCondition("id", query.getId());
+				condition.addCondition("accountNo", query.getAccountNo());
+				condition.addCondition("name", query.getName());
+				condition.addCondition("account", query.getAccount());
+				condition.addCondition("functionId", query.getFunctionId());
+				condition.setPage(query);
+			}
+			List<SystemAccountEntity> list = accountDao
+					.querySystemAccount(condition);
+			query.setPage(condition);
+			ResultList<SystemAccountEntity> res = new ResultListImpl<SystemAccountEntity>();
+			res.setResults(list);
+			return res;
+		} catch (Exception e) {
+			throw new ServiceException("查询系统用户错误", e);
+		}
+	}
+
+	@Override
+	@Transactional
+	public String initPassword(String accountId) throws ServiceException {
+		try {
+			int rondom = NumberUtils.getRandomIntInMax(999999);
+			String password = StringUtils.getLengthStr(rondom + "", 6, '0');
+			SystemAccountEntity account = new SystemAccountEntity();
+			account.setId(accountId);
+			account.setPassword(EncryptUtil.md5Hex(password));
+			accountDao.updateSystemAccount(account);
+			return password;
+		} catch (Exception e) {
+			throw new ServiceException("初始化系统用户密码错误", e);
+		}
+	}
+
+	@Override
+	public void updateLoginTime(String accountId) throws ServiceException {
+		try {
+			SystemAccountEntity account = new SystemAccountEntity();
+			account.setId(accountId);
+			account.setLastLogin(new Date());
+			accountDao.updateSystemAccount(account);
+		} catch (Exception e) {
+			throw new ServiceException("更新系統用戶登錄時間错误", e);
+		}
+
+	}
+
+	@Override
+	public void edit_pwd(String id, String password) throws ServiceException {
+		try {
+			SystemAccountEntity account = new SystemAccountEntity();
+			account.setId(id);
+			account.setPassword(password);
+			if (StringUtils.isNotEmpty(account.getPassword())) {
+				account.setPassword(EncryptUtil.md5Hex(account.getPassword()));
+			}
+			accountDao.updateSystemAccount(account);
+		} catch (SQLException e) {
+			throw new ServiceException("修改密码错误", e);
+		}
+	}
+
 }
