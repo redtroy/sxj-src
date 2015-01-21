a21962157e1c803d10ac16106b7f507c63aa9286
djx <djx@sxj.com>
2015-01-21 18:59:20 +0800

    Change-Id: I5c2c535c41290d2c88253938fd855578b9d8eed7

--- supervisor-parent/supervisor-website/src/test/java/com/sxj/statemachine/StateMachineTest.java
+++ supervisor-parent/supervisor-website/src/test/java/com/sxj/statemachine/StateMachineTest.java
@@ -12,26 +12,24 @@ import com.sxj.statemachine.exceptions.StateMachineException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/applicationContext-test.xml" })
-public class StateMachineTest
-{
-    @Autowired
-    StateMachineImpl<DemoStates> fsm;
-    
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception
-    {
-    }
-    
-    @After
-    public void tearDown() throws Exception
-    {
-    }
-    
-    @Test
-    public void test() throws StateMachineException
-    {
-        fsm.fire("AtoB", null);
-        org.junit.Assert.assertEquals(DemoStates.B, fsm.getCurrentState());
-    }
-    
+public class StateMachineTest {
+	@Autowired
+	StateMachineImpl<DemoStates> fsm;
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws StateMachineException {
+		// fsm.fire("AtoB", null);
+		fsm.setCurrentState(DemoStates.B);
+		fsm.fire("BtoC", null);
+		System.out.println(fsm.getCurrentState());
+		org.junit.Assert.assertEquals(DemoStates.C, fsm.getCurrentState());
+	}
 }
