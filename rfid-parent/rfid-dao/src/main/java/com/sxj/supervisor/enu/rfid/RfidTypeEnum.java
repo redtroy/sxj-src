7e6ca6bf2a52999b8e1d0f26a7480665162718f7
ann <ann@sxj.com>
2015-01-27 14:53:38 +0800

    2015/1/27

--- rfid-parent/rfid-dao/src/main/java/com/sxj/supervisor/enu/rfid/RfidTypeEnum.java
+++ rfid-parent/rfid-dao/src/main/java/com/sxj/supervisor/enu/rfid/RfidTypeEnum.java
@@ -1,38 +1,45 @@
 package com.sxj.supervisor.enu.rfid;
 
-public enum RfidTypeEnum
-{
-    DOOR("门窗标签", 0), GLASS("玻璃标签", 1), EXTRUSIONS("型材标签", 2);
-    
-    // 成员变量
-    private Integer id;
-    
-    private String name;
-    
-    private RfidTypeEnum(String name, Integer id)
-    {
-        this.name = name;
-        this.id = id;
-    }
-    
-    
-    public Integer getId()
-    {
-        return id;
-    }
-    
-    public void setId(Integer id)
-    {
-        this.id = id;
-    }
-    
-    public String getName()
-    {
-        return name;
-    }
-    
-    public void setName(String name)
-    {
-        this.name = name;
-    }
+public enum RfidTypeEnum {
+	DOOR("门窗标签", 0), GLASS("玻璃标签", 1), EXTRUSIONS("型材标签", 2);
+
+	// 成员变量
+	private Integer id;
+
+	private String name;
+
+	private RfidTypeEnum(String name, Integer id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	// 普通方法
+	public static String getName(Integer id) {
+		if (id == null) {
+				return null;
+			}
+		for (RfidTypeEnum c : RfidTypeEnum.values()) {
+			
+			if (c.getId().equals(id)) {
+				return c.name;
+			}
+		}
+		return null;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
