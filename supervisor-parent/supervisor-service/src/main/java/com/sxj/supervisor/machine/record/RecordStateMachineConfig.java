b645f3bccbf53e134ef2d13ac93fb548e587a7b6
redtroy <redtroy@sxj.com>
2015-01-29 18:31:21 +0800

    Change-Id: I2a1676fe5362aa0d3e3ed3ce0febdbffc78a2bbf

--- supervisor-parent/supervisor-service/src/main/java/com/sxj/supervisor/machine/record/RecordStateMachineConfig.java
+++ supervisor-parent/supervisor-service/src/main/java/com/sxj/supervisor/machine/record/RecordStateMachineConfig.java
@@ -13,32 +13,32 @@ import com.sxj.supervisor.entity.record.RecordEntity;
 import com.sxj.supervisor.enu.record.RecordStateEnum;
 
 @StateMachine(stateType = RecordStateEnum.class, startState = "NOBINDING", finalStates = { "BINDING" })
-public class RecordStateMachineConfig {
-
-	@Autowired
-	private IRecordDao recordDao;
-
-	@Transitions({ @Transition(source = "NOBINDING", event = "NOBINDING", target = "BINDING") })
-	public void noop(TransitionInfo event) {
-		System.out.println("tx@:" + event.getEvent());
-	}
-
-	@Transitions({ @Transition(source = "BINDING", event = "BINDING", target = "BINDING") })
-	public void noop2(TransitionInfo event) {
-		System.out.println("tx@:" + event.getEvent());
-	}
-
-	@OnEnter(value = "BINDING")
-	public EventInfo enterB(TransitionInfo event) {
-		RecordEntity record = (RecordEntity) event.getObject();
-		record.setState(RecordStateEnum.BINDING);
-		// recordDao.updateRecord(record);
-		return null;
-	}
-	//
-	// @OnExit("A")
-	// public Boolean exitA(TransitionInfo event) {
-	// System.out.println("Exited A state");
-	// return true;
-	// }
+public class RecordStateMachineConfig
+{
+    
+    @Autowired
+    private IRecordDao recordDao;
+    
+    @Transitions({
+            @Transition(source = "NOBINDING", event = "NOBINDING", target = "BINDING"),
+            @Transition(source = "BINDING", event = "BINDING", target = "BINDING") })
+    public void noop(TransitionInfo event)
+    {
+        System.out.println("tx@:" + event.getEvent());
+    }
+    
+    @OnEnter(value = "BINDING")
+    public EventInfo enterB(TransitionInfo event)
+    {
+        RecordEntity record = (RecordEntity) event.getObject();
+        record.setState(RecordStateEnum.BINDING);
+        // recordDao.updateRecord(record);
+        return null;
+    }
+    //
+    // @OnExit("A")
+    // public Boolean exitA(TransitionInfo event) {
+    // System.out.println("Exited A state");
+    // return true;
+    // }
 }
