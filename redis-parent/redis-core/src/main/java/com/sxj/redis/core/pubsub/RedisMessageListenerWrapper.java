239a193262d785b9e12b70a2ee30957bb5266acb
djx <djx@sxj.com>
2015-02-02 18:49:44 +0800

    Change-Id: I638f0f6813715213a382a96c72bdfcfc9f30bb6e

--- redis-parent/redis-core/src/main/java/com/sxj/redis/core/pubsub/RedisMessageListenerWrapper.java
+++ redis-parent/redis-core/src/main/java/com/sxj/redis/core/pubsub/RedisMessageListenerWrapper.java
@@ -9,74 +9,63 @@ import com.sxj.redis.core.MessageListener;
 import com.sxj.redis.core.serializer.JdkSerializer;
 import com.sxj.redis.core.serializer.Serializer;
 
-public class RedisMessageListenerWrapper<M> extends JedisPubSub
-{
-    private static final Logger LOGGER = LoggerFactory.getLogger(RedisMessageListenerWrapper.class);
-    
-    private MessageListener<M> listener;
-    
-    private String channel;
-    
-    protected final static Serializer V_SERIALIZER = new JdkSerializer();
-    
-    public RedisMessageListenerWrapper(MessageListener<M> listener,
-            String channel)
-    {
-        super();
-        this.listener = listener;
-        this.channel = channel;
-    }
-    
-    @Override
-    public void onMessage(String channel, String message)
-    {
-        try
-        {
-            listener.onMessage((M) V_SERIALIZER.deserialize(message));
-        }
-        catch (Exception e)
-        {
-            LOGGER.debug(message, e);
-        }
-    }
-    
-    @Override
-    public void onPMessage(String pattern, String channel, String message)
-    {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    @Override
-    public void onSubscribe(String channel, int subscribedChannels)
-    {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    @Override
-    public void onUnsubscribe(String channel, int subscribedChannels)
-    {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    @Override
-    public void onPUnsubscribe(String pattern, int subscribedChannels)
-    {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    @Override
-    public void onPSubscribe(String pattern, int subscribedChannels)
-    {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    public String getChannel()
-    {
-        return channel;
-    }
+public class RedisMessageListenerWrapper<M> extends JedisPubSub {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(RedisMessageListenerWrapper.class);
+
+	private MessageListener<M> listener;
+
+	private String channel;
+
+	protected final static Serializer V_SERIALIZER = new JdkSerializer();
+
+	public RedisMessageListenerWrapper(MessageListener<M> listener,
+			String channel) {
+		super();
+		this.listener = listener;
+		this.channel = channel;
+	}
+
+	@Override
+	public void onMessage(String channel, String message) {
+		try {
+			listener.onMessage((M) V_SERIALIZER.deserialize(message));
+		} catch (Exception e) {
+			LOGGER.debug(message, e);
+		}
+	}
+
+	@Override
+	public void onPMessage(String pattern, String channel, String message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSubscribe(String channel, int subscribedChannels) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUnsubscribe(String channel, int subscribedChannels) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPUnsubscribe(String pattern, int subscribedChannels) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPSubscribe(String pattern, int subscribedChannels) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getChannel() {
+		return channel;
+	}
 }
