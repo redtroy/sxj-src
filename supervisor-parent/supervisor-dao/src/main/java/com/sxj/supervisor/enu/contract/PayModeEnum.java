5515250ac81ad9cc102794bc282b2dc5b99b4c53
redtroy <redtroy@sxj.com>
2015-02-06 16:22:43 +0800

    Signed-off-by: redtroy <redtroy@sxj.com>

--- supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/enu/contract/PayModeEnum.java
+++ supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/enu/contract/PayModeEnum.java
@@ -1,36 +1,30 @@
 package com.sxj.supervisor.enu.contract;
 
-public enum PayModeEnum
-{
-    MODE1("待支付", "未付款"), MODE2_0("融资受理中", "支付中"), MODE2_1("融资已搁置", "支付中"), MODE2_2(
-            "融资已放款", "支付中"), MODE3("现金支付", ""), MODE4("融资支付", "");
-    private String nameA;
-    
-    private String nameB;
-    
-    private PayModeEnum(String nameA, String nameB)
-    {
-        this.nameA = nameA;
-        this.nameB = nameB;
-    }
-    
-    public String getNameA()
-    {
-        return nameA;
-    }
-    
-    public void setNameA(String nameA)
-    {
-        this.nameA = nameA;
-    }
-    
-    public String getNameB()
-    {
-        return nameB;
-    }
-    
-    public void setNameB(String nameB)
-    {
-        this.nameB = nameB;
-    }
+public enum PayModeEnum {
+	MODE1("待支付", "未付款"), MODE2_0("融资受理中", "支付中"), MODE2_1("融资已搁置", "支付中"), MODE2_2(
+			"融资已放款", "支付中"), MODE3("现金支付", ""), MODE4("融资支付", "");
+	private String nameA;
+
+	private String nameB;
+
+	private PayModeEnum(String nameA, String nameB) {
+		this.nameA = nameA;
+		this.nameB = nameB;
+	}
+
+	public String getNameA() {
+		return nameA;
+	}
+
+	public void setNameA(String nameA) {
+		this.nameA = nameA;
+	}
+
+	public String getNameB() {
+		return nameB;
+	}
+
+	public void setNameB(String nameB) {
+		this.nameB = nameB;
+	}
 }
