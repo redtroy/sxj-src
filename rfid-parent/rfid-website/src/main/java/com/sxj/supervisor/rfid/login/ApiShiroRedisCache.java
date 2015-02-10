a79ee1c2681acdd6f469378417230d2a4c391c31
redtroy <redtroy@sxj.com>
2015-02-10 10:51:59 +0800

    Signed-off-by: redtroy <redtroy@sxj.com>

--- rfid-parent/rfid-website/src/main/java/com/sxj/supervisor/rfid/login/ApiShiroRedisCache.java
+++ rfid-parent/rfid-website/src/main/java/com/sxj/supervisor/rfid/login/ApiShiroRedisCache.java
@@ -5,58 +5,65 @@ import java.util.List;
 
 import org.apache.shiro.cache.CacheException;
 
+import com.sxj.cache.manager.CacheLevel;
 import com.sxj.redis.core.RMap;
 import com.sxj.redis.core.collections.RedisCollections;
 import com.sxj.spring.modules.security.shiro.ShiroRedisCache;
 import com.sxj.util.Constraints;
 
-public class ApiShiroRedisCache<K, V> extends ShiroRedisCache<K, V> {
-
-	private static RedisCollections collections;
-
-	public void setCollections(RedisCollections collections) {
-		ApiShiroRedisCache.collections = collections;
-	}
-
-	public ApiShiroRedisCache(int level, String name) {
-		super(level, name);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public V get(K key) throws CacheException {
-		return super.get(key);
-	}
-
-	@Override
-	public V put(K key, V value) throws CacheException {
-		return super.put(key, value);
-	}
-
-	public static void addToMap(String accountNo, Object authorizationKey) {
-		RMap<Object, Object> map = collections
-				.getMap(Constraints.SHIRO_MAP_KEY);
-		List<Object> object = (List<Object>) map.get(accountNo);
-		if (object == null) {
-			object = new ArrayList<Object>();
-		}
-		object.add(authorizationKey);
-		map.put(accountNo, object);
-	}
-
-	@Override
-	public V remove(K key) throws CacheException {
-		// TODO Auto-generated method stub
-		return super.remove(key);
-	}
-
-	@Override
-	public void clear() throws CacheException {
-		// TODO Auto-generated method stub
-		super.clear();
-		RMap<Object, Object> map = collections
-				.getMap(Constraints.SHIRO_MAP_KEY);
-		map.clear();
-	}
-
+public class ApiShiroRedisCache<K, V> extends ShiroRedisCache<K, V>
+{
+    
+    private static RedisCollections collections;
+    
+    public void setCollections(RedisCollections collections)
+    {
+        ApiShiroRedisCache.collections = collections;
+    }
+    
+    public ApiShiroRedisCache(CacheLevel level, String name)
+    {
+        super(level, name);
+    }
+    
+    @Override
+    public V get(K key) throws CacheException
+    {
+        return super.get(key);
+    }
+    
+    @Override
+    public V put(K key, V value) throws CacheException
+    {
+        return super.put(key, value);
+    }
+    
+    public static void addToMap(String accountNo, Object authorizationKey)
+    {
+        RMap<Object, Object> map = collections.getMap(Constraints.SHIRO_MAP_KEY);
+        List<Object> object = (List<Object>) map.get(accountNo);
+        if (object == null)
+        {
+            object = new ArrayList<Object>();
+        }
+        object.add(authorizationKey);
+        map.put(accountNo, object);
+    }
+    
+    @Override
+    public V remove(K key) throws CacheException
+    {
+        // TODO Auto-generated method stub
+        return super.remove(key);
+    }
+    
+    @Override
+    public void clear() throws CacheException
+    {
+        // TODO Auto-generated method stub
+        super.clear();
+        RMap<Object, Object> map = collections.getMap(Constraints.SHIRO_MAP_KEY);
+        map.clear();
+    }
+    
 }
--- rfid-parent/rfid-website/src/main/java/com/sxj/supervisor/rfid/login/ApiShiroRedisCacheManager.java
+++ rfid-parent/rfid-website/src/main/java/com/sxj/supervisor/rfid/login/ApiShiroRedisCacheManager.java
@@ -3,24 +3,40 @@ package com.sxj.supervisor.rfid.login;
 import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheException;
 
+import com.sxj.cache.manager.CacheLevel;
 import com.sxj.redis.core.collections.RedisCollections;
 import com.sxj.spring.modules.security.shiro.ShiroRedisCacheManager;
 
-public class ApiShiroRedisCacheManager extends ShiroRedisCacheManager {
-	private RedisCollections collections;
-
-	@Override
-	protected Cache<String, Object> createCache(String cacheName)
-			throws CacheException {
-		// TODO Auto-generated method stub
-		ApiShiroRedisCache<String, Object> supervisorShiroRedisCache = new ApiShiroRedisCache<String, Object>(
-				getLevel(), cacheName);
-		supervisorShiroRedisCache.setCollections(collections);
-		return supervisorShiroRedisCache;
-	}
-
-	public void setCollections(RedisCollections collections) {
-		this.collections = collections;
-	}
-
+public class ApiShiroRedisCacheManager extends ShiroRedisCacheManager
+{
+    private RedisCollections collections;
+    
+    private CacheLevel level;
+    
+    @Override
+    protected Cache<String, Object> createCache(String cacheName)
+            throws CacheException
+    {
+        // TODO Auto-generated method stub
+        ApiShiroRedisCache<String, Object> supervisorShiroRedisCache = new ApiShiroRedisCache<String, Object>(
+                getLevel(), cacheName);
+        supervisorShiroRedisCache.setCollections(collections);
+        return supervisorShiroRedisCache;
+    }
+    
+    public void setCollections(RedisCollections collections)
+    {
+        this.collections = collections;
+    }
+    
+    public CacheLevel getLevel()
+    {
+        return level;
+    }
+    
+    public void setLevel(CacheLevel level)
+    {
+        this.level = level;
+    }
+    
 }
