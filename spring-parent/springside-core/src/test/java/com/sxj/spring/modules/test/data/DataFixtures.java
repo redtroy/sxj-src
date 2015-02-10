17a9965f99e39c612a37d65bec5e5169d36de44c
redtroy <redtroy@sxj.com>
2015-02-10 16:19:53 +0800

    Signed-off-by: redtroy <redtroy@sxj.com>

--- spring-parent/springside-core/src/test/java/com/sxj/spring/modules/test/data/DataFixtures.java
+++ spring-parent/springside-core/src/test/java/com/sxj/spring/modules/test/data/DataFixtures.java
@@ -18,19 +18,23 @@ import org.springframework.test.jdbc.JdbcTestUtils;
 /**
  * SQL数据文件导入工具类。
  * 
- * @author Calvin
  */
-public class DataFixtures {
-
-	public static final String DEFAULT_ENCODING = "UTF-8";
-
-	private static ResourceLoader resourceLoader = new DefaultResourceLoader();
-
-	public static void executeScript(DataSource dataSource, String... sqlResourcePaths) throws DataAccessException {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		for (String sqlResourcePath : sqlResourcePaths) {
-			Resource resource = resourceLoader.getResource(sqlResourcePath);
-			JdbcTestUtils.executeSqlScript(jdbcTemplate, new EncodedResource(resource, DEFAULT_ENCODING), true);
-		}
-	}
+public class DataFixtures
+{
+    
+    public static final String DEFAULT_ENCODING = "UTF-8";
+    
+    private static ResourceLoader resourceLoader = new DefaultResourceLoader();
+    
+    public static void executeScript(DataSource dataSource,
+            String... sqlResourcePaths) throws DataAccessException
+    {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        for (String sqlResourcePath : sqlResourcePaths)
+        {
+            Resource resource = resourceLoader.getResource(sqlResourcePath);
+            JdbcTestUtils.executeSqlScript(jdbcTemplate, new EncodedResource(
+                    resource, DEFAULT_ENCODING), true);
+        }
+    }
 }
