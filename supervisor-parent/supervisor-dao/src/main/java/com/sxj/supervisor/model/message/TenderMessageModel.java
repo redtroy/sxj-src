cadc000311627fb6e35dc81d893871e9b1be4424
djx <djx@sxj.com>
2015-03-23 14:39:49 +0800

    Change-Id: I1c8b2bb3037c61164f10685bf0a1d9dceefd6601

---
+++ supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/model/message/TenderMessageModel.java
@@ -0,0 +1,72 @@
+package com.sxj.supervisor.model.message;
+
+import java.io.Serializable;
+
+import com.sxj.mybatis.pagination.Pagable;
+import com.sxj.supervisor.entity.gather.WindDoorEntity;
+import com.sxj.supervisor.entity.message.TenderMessageEntity;
+import com.sxj.supervisor.enu.message.MessageStateEnum;
+
+public class TenderMessageModel extends Pagable implements Serializable
+{
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 5123146449337291079L;
+    
+    private WindDoorEntity info = new WindDoorEntity();
+    
+    private TenderMessageEntity message = new TenderMessageEntity();
+    
+    public String getId()
+    {
+        return message.getId();
+    }
+    
+    public void setId(String id)
+    {
+        message.setId(id);
+    }
+    
+    public String getInfoId()
+    {
+        return message.getInfoId();
+    }
+    
+    public void setInfoId(String infoId)
+    {
+        message.setInfoId(infoId);
+    }
+    
+    public MessageStateEnum getState()
+    {
+        return message.getState();
+    }
+    
+    public void setState(MessageStateEnum state)
+    {
+        message.setState(state);
+    }
+    
+    public String getXmmc()
+    {
+        return info.getXmmc();
+    }
+    
+    public void setXmmc(String xmmc)
+    {
+        info.setXmmc(xmmc);
+    }
+    
+    public String getMemberNo()
+    {
+        return message.getMemberNo();
+    }
+    
+    public void setMemberNo(String memberNo)
+    {
+        message.setMemberNo(memberNo);
+    }
+    
+}
