900bbfe600ac3333ac6d9fd55f746d813ca5e257
djx <djx@sxj.com>
2015-03-25 18:48:35 +0800

    Change-Id: Ife9693b1baadff1842d440586b462019ecb55197

--- supervisor-parent/supervisor-website/src/test/java/com/sxj/supervisor/jsoup/SpringJsoupTest.java
+++ supervisor-parent/supervisor-website/src/test/java/com/sxj/supervisor/jsoup/SpringJsoupTest.java
@@ -4,32 +4,33 @@ import java.io.FileNotFoundException;
 
 import org.jsoup.nodes.Entities;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternUtils;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.sxj.spring.modules.util.Reflections;
 import com.sxj.supervisor.service.tasks.impl.WindDoorServiceImpl;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring/applicationContext_2.xml" })
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "classpath:spring/applicationContext_2.xml" })
 public class SpringJsoupTest
 {
     @Autowired
     WindDoorServiceImpl service;
     
-    @Test
     public void test()
     {
         service.WindDoorGather();
     }
     
-    public void testClassLoader() throws FileNotFoundException
+    @Test
+    public void testClassLoader() throws FileNotFoundException,
+            InstantiationException, IllegalAccessException
     {
         ResourcePatternUtils.getResourcePatternResolver(new PathMatchingResourcePatternResolver())
-                .getResource("/entities-base.properties");
-        Entities.class.getResource("/entities-base.properties");
+                .getResource("classpath*:/entities-base.properties");
+        Reflections.invokeMethodByName(Entities.class.newInstance(),
+                "loadProperties",
+                new String[] { "entities-base.properties" });
     }
 }
