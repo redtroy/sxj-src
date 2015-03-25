fc07949533c58e1ef792175a3de9495a5873b395
qjj[merge] <qjj@sxj.com>
2015-03-25 11:32:17 +0800

    Signed-off-by: qjj[merge] <qjj@sxj.com>

--- supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/message/WindDoorInfoController.java
+++ supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/message/WindDoorInfoController.java
@@ -12,21 +12,25 @@ import com.sxj.util.logger.SxjLogger;
 
 @Controller
 @RequestMapping("market")
-public class WindDoorInfoController {
-
-	@Autowired
-	private IWindDoorService iwds;
-
-	@RequestMapping("info")
-	public String info(ModelMap map, String id) throws WebException {
-		try {
-			WindDoorEntity wde = iwds
-					.getInfoById("1xBkR9UNnt6W2jUT5b7VgFkuNUgNd4wF");
-			map.put("model", wde);
-		} catch (Exception e) {
-			SxjLogger.error("查询门窗工程详细信息出错", e, this.getClass());
-			throw new WebException(e.getMessage());
-		}
-		return "site/message/invite-info";
-	}
+public class WindDoorInfoController
+{
+    
+    @Autowired
+    private IWindDoorService iwds;
+    
+    @RequestMapping("info")
+    public String info(ModelMap map, String id) throws WebException
+    {
+        try
+        {
+            WindDoorEntity wde = iwds.getInfoById(id);
+            map.put("model", wde);
+        }
+        catch (Exception e)
+        {
+            SxjLogger.error("查询门窗工程详细信息出错", e, this.getClass());
+            throw new WebException(e.getMessage());
+        }
+        return "site/message/invite-info";
+    }
 }
