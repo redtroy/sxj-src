7a2c7307788007fa28fcadace7c370f4b7e3f9f2
qjj[merge] <qjj@sxj.com>
2015-04-07 15:57:35 +0800

    Signed-off-by: qjj[merge] <qjj@sxj.com>

--- supervisor-parent/supervisor-tasks/src/main/java/com/sxj/supervisor/service/tasks/impl/WindDoorServiceImpl.java
+++ supervisor-parent/supervisor-tasks/src/main/java/com/sxj/supervisor/service/tasks/impl/WindDoorServiceImpl.java
@@ -156,7 +156,7 @@ public class WindDoorServiceImpl implements IWindDoorService
                     }
                     bathList.add(windDoor);
                     //发短信
-                    configService.sendAllMessage("您有一条新的开发商招标信息，请登录私享家绿色门窗平台查看详情！");
+                    configService.sendAllMessage("您有一条新的开发商招标信息");
                     CometServiceImpl.add(MessageChannel.MEMBER_TENDER_MESSAGE_INFO,
                             windDoor.getId());
                     flag++;
