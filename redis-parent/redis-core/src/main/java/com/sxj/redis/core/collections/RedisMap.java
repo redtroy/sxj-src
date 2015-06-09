e197f6a3485ce335e5f6a36f595278c909457d5b
zhangjian <zhangjian@sxj.com>
2015-06-09 10:09:22 +0800

    Change-Id: Ie4f9abf7d68a1a5f28216a51658369cc63126537

--- redis-parent/redis-core/src/main/java/com/sxj/redis/core/collections/RedisMap.java
+++ redis-parent/redis-core/src/main/java/com/sxj/redis/core/collections/RedisMap.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import java.util.Set;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Transaction;
 
 import com.sxj.redis.core.RMap;
 import com.sxj.redis.core.exception.RedisException;
@@ -49,11 +48,11 @@ public class RedisMap<K, V> extends RedisExpirable implements RMap<K, V>
         }
     }
     
-    private boolean isEqual(Transaction multi, Object key, Object object)
+    private boolean isEqual(Jedis jedis, Object key, Object object)
     {
-        if (multi.exists(K_SERIALIZER.serialize(key)).get())
+        if (jedis.hexists(name, K_SERIALIZER.serialize(key)))
         {
-            String hget = multi.hget(name, K_SERIALIZER.serialize(key)).get();
+            String hget = jedis.hget(name, K_SERIALIZER.serialize(key));
             Object deserialize = V_SERIALIZER.deserialize(hget);
             return deserialize.equals(object);
         }
@@ -68,17 +67,14 @@ public class RedisMap<K, V> extends RedisExpirable implements RMap<K, V>
         boolean retValue = false;
         try
         {
-            Transaction multi = jedis.multi();
             
-            if (isEqual(multi, key, value))
+            if (isEqual(jedis, key, value))
             {
-                int intValue = multi.hdel(name, K_SERIALIZER.serialize(key))
-                        .get()
+                int intValue = jedis.hdel(name, K_SERIALIZER.serialize(key))
                         .intValue();
                 if (intValue == 1)
                     retValue = true;
             }
-            multi.exec();
         }
         catch (Exception e)
         {
@@ -100,14 +96,11 @@ public class RedisMap<K, V> extends RedisExpirable implements RMap<K, V>
         boolean broken = false;
         try
         {
-            Transaction multi = jedis.multi();
-            if (isEqual(multi, key, oldValue))
+            if (isEqual(jedis, key, oldValue))
             {
-                int intValue = multi.hset(name,
+                int intValue = jedis.hset(name,
                         K_SERIALIZER.serialize(key),
-                        V_SERIALIZER.serialize(newValue))
-                        .get()
-                        .intValue();
+                        V_SERIALIZER.serialize(newValue)).intValue();
                 if (intValue == 1)
                     retValue = true;
             }
@@ -131,12 +124,9 @@ public class RedisMap<K, V> extends RedisExpirable implements RMap<K, V>
         boolean broken = false;
         try
         {
-            Transaction multi = jedis.multi();
-            int intValue = multi.hset(name,
+            int intValue = jedis.hset(name,
                     K_SERIALIZER.serialize(key),
-                    V_SERIALIZER.serialize(value))
-                    .get()
-                    .intValue();
+                    V_SERIALIZER.serialize(value)).intValue();
             if (intValue == 1)
                 return value;
             return null;
@@ -281,14 +271,14 @@ public class RedisMap<K, V> extends RedisExpirable implements RMap<K, V>
         boolean broken = false;
         try
         {
-            Transaction multi = jedis.multi();
             V retValue = null;
             String serializeKey = K_SERIALIZER.serialize(key);
-            if (multi.exists(serializeKey).get())
+            
+            if (jedis.hexists(name, serializeKey))
             {
-                V deserialize = (V) V_SERIALIZER.deserialize(multi.hget(name,
-                        serializeKey).get());
-                int hdel = multi.hdel(name, serializeKey).get().intValue();
+                V deserialize = (V) V_SERIALIZER.deserialize(jedis.hget(name,
+                        serializeKey));
+                int hdel = jedis.hdel(name, serializeKey).intValue();
                 if (hdel == 1)
                     retValue = deserialize;
             }
@@ -297,7 +287,7 @@ public class RedisMap<K, V> extends RedisExpirable implements RMap<K, V>
         catch (Exception e)
         {
             broken = true;
-            throw new RedisException("", e);
+            throw new RedisException(e);
         }
         finally
         {
--- supervisor-parent/supervisor-manager/src/test/java/com/sxj/supervisor/dao/IRoleDAOTest.java
+++ supervisor-parent/supervisor-manager/src/test/java/com/sxj/supervisor/dao/IRoleDAOTest.java
@@ -1,9 +1,11 @@
 package com.sxj.supervisor.dao;
 
+import org.apache.shiro.cache.CacheManager;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -19,18 +21,29 @@ public class IRoleDAOTest
     @Autowired
     IRoleDao roleDao;
     
+    @Autowired
+    @Qualifier("shiroCacheManager")
+    CacheManager shiroCacheManager;
+    
     @AfterClass
     public static void tearDownAfterClass() throws Exception
     {
     }
     
-    @Test
     public void test()
     {
         RoleEntity role = new RoleEntity();
         RoleEntity role1 = new RoleEntity();
         RoleEntity[] roles = new RoleEntity[] { role, role1 };
         roleDao.addRoles(roles);
+    }
+    
+    @Test
+    public void testDeleteSession()
+    {
+        shiroCacheManager.getCache("manage.session.cache.name")
+                .remove("fe5bac9e-489f-46b8-866b-80e287ff9917");
+        
     }
     
 }
