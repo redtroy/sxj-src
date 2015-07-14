b2024bfefcd2ed6e6d1c327701717e1d80bab781
zhangjian <zhangjian@sxj.com>
2015-07-14 12:24:01 +0800

    Change-Id: I46040226710b6e56b4bd971c7a3120be5e29f95d

--- jsonrpc-parent/jsonrpc-server/src/main/java/com/sxj/jsonrpc/server/JsonRpcServer.java
+++ jsonrpc-parent/jsonrpc-server/src/main/java/com/sxj/jsonrpc/server/JsonRpcServer.java
@@ -1,27 +1,3 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2014 jsonrpc4j
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
- */
-
 package com.sxj.jsonrpc.server;
 
 import static com.sxj.jsonrpc.core.ReflectionUtil.findMethods;
@@ -73,10 +49,6 @@ import com.sxj.jsonrpc.core.MultipleErrorResolver;
 import com.sxj.jsonrpc.core.NoCloseOutputStream;
 import com.sxj.jsonrpc.core.ReadContext;
 
-/**
- * A JSON-RPC request server reads JSON-RPC requests from an
- * input stream and writes responses to an output stream.
- */
 public class JsonRpcServer
 {
     
