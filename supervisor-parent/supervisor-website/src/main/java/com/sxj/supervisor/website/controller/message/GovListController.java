2c6c154222ffc517957d454c88894d9890aed42f
nist <nishaotang@126.com>
2015-08-12 10:24:33 +0800

    Signed-off-by: nist <nishaotang@126.com>

--- supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/message/GovListController.java
+++ supervisor-parent/supervisor-website/src/main/java/com/sxj/supervisor/website/controller/message/GovListController.java
@@ -2,27 +2,19 @@ package com.sxj.supervisor.website.controller.message;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sxj.supervisor.model.login.SupervisorPrincipal;
 import com.sxj.supervisor.model.message.TenderMessageQuery;
-import com.sxj.supervisor.service.message.IMessageConfigService;
-import com.sxj.supervisor.service.message.ITenderMessageService;
 import com.sxj.supervisor.website.controller.BaseController;
 import com.sxj.util.exception.WebException;
 
 @Controller
 @RequestMapping("/government")
-public class GovernmentMessageController extends BaseController
+public class GovListController extends BaseController
 {
-    @Autowired
-    private ITenderMessageService service;
-    
-    @Autowired
-    private IMessageConfigService configService;
     
     @RequestMapping("/query")
     public String query(TenderMessageQuery query, ModelMap map,
@@ -34,7 +26,7 @@ public class GovernmentMessageController extends BaseController
             return LOGIN;
         }
         
-        return "site/message/government-message";
+        return "site/message/govlist";
     }
     
 }
