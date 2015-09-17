eabb9fa210454f3a6ff043c9be20bbe20c414652
qjj[merge] <qjj@sxj.com>
2015-09-17 14:30:01 +0800

    2015-9-17

--- supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/dao/member/ICertificateLevelDao.java
+++ supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/dao/member/ICertificateLevelDao.java
@@ -1,40 +1,41 @@
 package com.sxj.supervisor.dao.member;
 
 import java.sql.SQLException;
+import java.util.List;
 
-import com.sun.tools.javac.util.List;
 import com.sxj.mybatis.orm.annotations.Insert;
 import com.sxj.mybatis.orm.annotations.Update;
 import com.sxj.supervisor.entity.member.CertificateLevelEntity;
 
-public interface ICertificateLevelDao {
-
-	/**
-	 * 添加证书等级
-	 * 
-	 * @throws SQLException
-	 */
-	@Insert
-	public void addLevel(CertificateLevelEntity certificateLevelEntity)
-			throws SQLException;
-
-	/**
-	 * 更新证书等级
-	 * 
-	 * @throws SQLException
-	 */
-	@Update
-	public void updateLevel(CertificateLevelEntity certificateLevelEntity)
-			throws SQLException;
-
-	/**
-	 * 获取证书等级
-	 * 
-	 * @param imageId
-	 * @return
-	 * @throws SQLException
-	 */
-	public List<CertificateLevelEntity> getCertificateLevel(String imageId)
-			throws SQLException;
-
+public interface ICertificateLevelDao
+{
+    
+    /**
+     * 添加证书等级
+     * 
+     * @throws SQLException
+     */
+    @Insert
+    public void addLevel(CertificateLevelEntity certificateLevelEntity)
+            throws SQLException;
+    
+    /**
+     * 更新证书等级
+     * 
+     * @throws SQLException
+     */
+    @Update
+    public void updateLevel(CertificateLevelEntity certificateLevelEntity)
+            throws SQLException;
+    
+    /**
+     * 获取证书等级
+     * 
+     * @param imageId
+     * @return
+     * @throws SQLException
+     */
+    public List<CertificateLevelEntity> getCertificateLevel(String imageId)
+            throws SQLException;
+    
 }
