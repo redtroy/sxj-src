58257d03e0bf4cdc762493eb67eba41aab0b067c
anshaoshuai <anshaoshuai@sxj.com>
2015-09-18 11:17:03 +0800

    2015/9/18

--- supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/dao/member/ICertificateLevelDao.java
+++ supervisor-parent/supervisor-dao/src/main/java/com/sxj/supervisor/dao/member/ICertificateLevelDao.java
@@ -1,8 +1,8 @@
 package com.sxj.supervisor.dao.member;
 
 import java.sql.SQLException;
+import java.util.List;
 
-import com.sun.tools.javac.util.List;
 import com.sxj.mybatis.orm.annotations.Insert;
 import com.sxj.mybatis.orm.annotations.Update;
 import com.sxj.supervisor.entity.member.CertificateLevelEntity;
