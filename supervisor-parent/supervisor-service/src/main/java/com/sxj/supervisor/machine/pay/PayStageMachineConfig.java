4149e0229ad79093000b5b30e5e4fafb84f1785d
zhangjian <zhangjian@sxj.com>
2015-12-09 17:55:06 +0800

    Signed-off-by: zhangjian <zhangjian@sxj.com>

--- supervisor-parent/supervisor-service/src/main/java/com/sxj/supervisor/machine/pay/PayStageMachineConfig.java
+++ supervisor-parent/supervisor-service/src/main/java/com/sxj/supervisor/machine/pay/PayStageMachineConfig.java
@@ -17,45 +17,56 @@ import com.sxj.supervisor.entity.pay.PayRecordEntity;
 import com.sxj.supervisor.enu.contract.PayModeEnum;
 import com.sxj.supervisor.enu.contract.PayStageEnum;
 
-@StateMachine(stateType = PayStageEnum.class, startState = "STAGE1", finalStates = { "STAGE3" }, name = "payStagefsm")
-public class PayStageMachineConfig {
-
-	@Autowired
-	@Qualifier("payModeFsm")
-	private StateMachineImpl<PayModeEnum> payModeFsm;
-
-	@Transitions({ @Transition(source = "STAGE1", event = "STAGE1_A", target = "STAGE2") })
-	public void noop(TransitionInfo event) {
-		System.out.println("tx@:" + event.getEvent());
-	}
-
-	@Transitions({ @Transition(source = "STAGE2", event = "STAGE2_B", target = "STAGE3") })
-	public void noop1(TransitionInfo event) {
-		System.out.println("tx@:" + event.getEvent());
-	}
-
-	@OnEnter(value = "STAGE2")
-	public EventInfo enter2(TransitionInfo event) {
-		PayRecordEntity re = (PayRecordEntity) event.getObject();
-		re.setState(PayStageEnum.STAGE2);
-		re.setPayDate(new Date());
-		return null;
-	}
-
-	@OnEnter(value = "STAGE3")
-	public EventInfo enter3(TransitionInfo event) {
-		PayRecordEntity re = (PayRecordEntity) event.getObject();
-		re.setState(PayStageEnum.STAGE3);
-		payModeFsm.setCurrentState(re.getPayMode());
-		try {
-			payModeFsm.fire(re.getPayMode().toString() + "_"
-					+ re.getState().toString() + "_", re);
-		} catch (StateMachineException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
+@StateMachine(stateType = PayStageEnum.class, startState = "STAGE1", finalStates = {
+        "STAGE3" }, name = "payStagefsm")
+public class PayStageMachineConfig
+{
+    
+    @Autowired
+    @Qualifier("payModeFsm")
+    private StateMachineImpl<PayModeEnum> payModeFsm;
+    
+    @Transitions({
+            @Transition(source = "STAGE1", event = "STAGE1_A", target = "STAGE2") })
+    public void noop(TransitionInfo event)
+    {
+        System.out.println("tx@:" + event.getEvent());
+    }
+    
+    @Transitions({
+            @Transition(source = "STAGE2", event = "STAGE2_B", target = "STAGE3") })
+    public void noop1(TransitionInfo event)
+    {
+        System.out.println("tx@:" + event.getEvent());
+    }
+    
+    @OnEnter(value = "STAGE2")
+    public EventInfo enter2(TransitionInfo event)
+    {
+        PayRecordEntity re = (PayRecordEntity) event.getObject();
+        re.setState(PayStageEnum.STAGE2);
+        re.setPayDate(new Date());
+        return null;
+    }
+    
+    @OnEnter(value = "STAGE3")
+    public EventInfo enter3(TransitionInfo event)
+    {
+        PayRecordEntity re = (PayRecordEntity) event.getObject();
+        re.setState(PayStageEnum.STAGE3);
+        payModeFsm.setCurrentState(re.getPayMode());
+        try
+        {
+            payModeFsm.fire(re.getPayMode().toString() + "_"
+                    + re.getState().toString() + "_", re);
+        }
+        catch (StateMachineException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+    
 }
--- supervisor-parent/supervisor-service/src/main/java/com/sxj/supervisor/machine/record/RecordStateMachineConfig.java
+++ supervisor-parent/supervisor-service/src/main/java/com/sxj/supervisor/machine/record/RecordStateMachineConfig.java
@@ -1,41 +1,39 @@
 package com.sxj.supervisor.machine.record;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.sxj.statemachine.EventInfo;
 import com.sxj.statemachine.TransitionInfo;
 import com.sxj.statemachine.annotations.OnEnter;
 import com.sxj.statemachine.annotations.StateMachine;
 import com.sxj.statemachine.annotations.Transition;
 import com.sxj.statemachine.annotations.Transitions;
-import com.sxj.supervisor.dao.record.IRecordDao;
 import com.sxj.supervisor.entity.record.RecordEntity;
 import com.sxj.supervisor.enu.record.RecordStateEnum;
 
-@StateMachine(stateType = RecordStateEnum.class, startState = "NOBINDING", finalStates = { "BINDING" }, name = "recordStatefsm")
-public class RecordStateMachineConfig {
-
-	@Autowired
-	private IRecordDao recordDao;
-
-	@Transitions({
-			@Transition(source = "NOBINDING", event = "NOBINDING", target = "BINDING"),
-			@Transition(source = "BINDING", event = "BINDING", target = "BINDING") })
-	public void noop(TransitionInfo event) {
-		System.out.println("tx@:" + event.getEvent());
-	}
-
-	@OnEnter(value = "BINDING")
-	public EventInfo enterB(TransitionInfo event) {
-		RecordEntity record = (RecordEntity) event.getObject();
-		record.setState(RecordStateEnum.BINDING);
-		// recordDao.updateRecord(record);
-		return null;
-	}
-	//
-	// @OnExit("A")
-	// public Boolean exitA(TransitionInfo event) {
-	// System.out.println("Exited A state");
-	// return true;
-	// }
+@StateMachine(stateType = RecordStateEnum.class, startState = "NOBINDING", finalStates = {
+        "BINDING" }, name = "recordStatefsm")
+public class RecordStateMachineConfig
+{
+    
+    @Transitions({
+            @Transition(source = "NOBINDING", event = "NOBINDING", target = "BINDING"),
+            @Transition(source = "BINDING", event = "BINDING", target = "BINDING") })
+    public void noop(TransitionInfo event)
+    {
+        System.out.println("tx@:" + event.getEvent());
+    }
+    
+    @OnEnter(value = "BINDING")
+    public EventInfo enterB(TransitionInfo event)
+    {
+        RecordEntity record = (RecordEntity) event.getObject();
+        record.setState(RecordStateEnum.BINDING);
+        // recordDao.updateRecord(record);
+        return null;
+    }
+    //
+    // @OnExit("A")
+    // public Boolean exitA(TransitionInfo event) {
+    // System.out.println("Exited A state");
+    // return true;
+    // }
 }
